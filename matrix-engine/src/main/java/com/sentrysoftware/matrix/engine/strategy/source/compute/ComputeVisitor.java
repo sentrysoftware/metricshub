a45b7c535e5390042c9fbbe259e8e8997546c4dd
Fadhela Kerdjoudj <fadhela@sentrysoftware.com>
2021-05-03 14:38:21 +0200

    MAT-47
    Implement the visitor of duplicateColumn source

--- matrix-engine/src/main/java/com/sentrysoftware/matrix/engine/strategy/source/compute/ComputeVisitor.java
+++ matrix-engine/src/main/java/com/sentrysoftware/matrix/engine/strategy/source/compute/ComputeVisitor.java
@@ -36,9 +36,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Slf4j
 public class ComputeVisitor implements IComputeVisitor {
 
 	@Getter
@@ -80,7 +82,24 @@ public class ComputeVisitor implements IComputeVisitor {
 
 	@Override
 	public void visit(final DuplicateColumn duplicateColumn) {
-		// Not implemented yet
+		if(duplicateColumn == null) {
+			log.debug("DuplicateColumn object is null, the table remains unchanged.");
+			return;
+		}
+		if(duplicateColumn.getColumn() == null || duplicateColumn.getColumn() == 0) {
+			log.debug("The column index in DuplicateColumn cannot be null or 0, the table remains unchanged.");
+			return;
+		}
+
+		// for each list in the list, duplicate the column of the given index  
+		Integer columnIndex = duplicateColumn.getColumn();
+
+		for (List<String> elementList : sourceTable.getTable()) {
+			if (columnIndex >= 1 && columnIndex <= elementList.size()) {
+				elementList.add(columnIndex - 1, elementList.get(columnIndex - 1));
+			}
+		}
+		
 	}
 
 	@Override
