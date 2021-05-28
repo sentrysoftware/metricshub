e318ec7ca113d0ee2d7f82cbdd10599e0459537d
Nassim Boutekedjiret <nassim@sentrysoftware.com>
2021-05-28 16:28:07 +0200

    MAT-124
    Handle Collect cycles
    
    * Added javadoc.

--- matrix-engine/src/main/java/com/sentrysoftware/matrix/common/helpers/JsonHelper.java
+++ matrix-engine/src/main/java/com/sentrysoftware/matrix/common/helpers/JsonHelper.java
@@ -17,13 +17,19 @@ public class JsonHelper {
 	public static <T> String serialize(final T value) {
 
 		try {
-			return getNewObjectMapper().writeValueAsString(value);
+			return buildObjectMapper().writeValueAsString(value);
 		} catch (JsonProcessingException e) {
 			return "{}";
 		}
 	}
 
-	public static ObjectMapper getNewObjectMapper() {
+	/**
+	 * Build and return a new {@link ObjectMapper} instance enabling the indentation.<br>
+	 * When no acessors are found for a type an empty Object is returned.
+	 * 
+	 * @return new {@link ObjectMapper} instance
+	 */
+	public static ObjectMapper buildObjectMapper() {
 		return new ObjectMapper()
 				.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
 				.enable(SerializationFeature.INDENT_OUTPUT);
