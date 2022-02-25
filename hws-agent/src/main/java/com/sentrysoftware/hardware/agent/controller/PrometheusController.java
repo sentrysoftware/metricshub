a5612ce45756bcd6efe68f02af2193d9995905c8
Nassim Boutekedjiret <nassim@sentrysoftware.com>
2022-02-25 17:29:34 +0100

    HWS-450
    [Internal] Update internal Matrix vulnerable dependencies
    
    * Upgraded Springboot to 2.6.4 as the old version comes with the
    vulnerable Spring Core.
    * Removed io.springfox and replaced with springdoc-openapi version 1.6.6

--- hws-agent/src/main/java/com/sentrysoftware/hardware/agent/controller/PrometheusController.java
+++ hws-agent/src/main/java/com/sentrysoftware/hardware/agent/controller/PrometheusController.java
@@ -1,8 +1,5 @@
 package com.sentrysoftware.hardware.agent.controller;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,12 +11,16 @@ import com.sentrysoftware.hardware.agent.dto.TargetContext;
 import com.sentrysoftware.hardware.agent.exception.BusinessException;
 import com.sentrysoftware.hardware.agent.service.prometheus.PrometheusService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 /**
  * REST Controllers for Prometheus operations.
  * 
  * @deprecated This controller is no longer supported as the agent pushes metrics to the OTEL Collector through OTLP
  */
-@Api(value = "/metrics")
+@Tag(name = "/metrics")
 @RestController
 @RequestMapping(value = "/", produces = MediaType.TEXT_PLAIN_VALUE)
 @Deprecated(since = "1.1")
@@ -29,10 +30,9 @@ public class PrometheusController {
 	private PrometheusService prometheusService;
 
 	@GetMapping("metrics")
-	@ApiOperation(
-			value = "Get hardware metrics in Prometheus format version 0.0.4.",
-			notes = "Get all the hardware metrics collected by Hardware Sentry Prometheus exporter.",
-			response = String.class
+	@Operation(
+			summary = "Get hardware metrics in Prometheus format version 0.0.4.",
+			description = "Get all the hardware metrics collected by Hardware Sentry Prometheus exporter."
 	)
 	public String metrics() throws BusinessException {
 
@@ -40,14 +40,13 @@ public class PrometheusController {
 	}
 
 	@GetMapping("metrics/{targetId}")
-	@ApiOperation(
-		value = "Get hardware metrics for a specific target in Prometheus format version 0.0.4.",
-		notes = "Get all the hardware metrics collected for a specific target by Hardware Sentry Prometheus exporter.",
-		response = String.class
+	@Operation(
+			summary = "Get hardware metrics for a specific target in Prometheus format version 0.0.4.",
+			description = "Get all the hardware metrics collected for a specific target by Hardware Sentry Prometheus exporter."
 	)
 	public String metrics(
 					@PathVariable("targetId")
-					@ApiParam(value = "The target identifier", example = "ecs1-01")
+					@Parameter(description = "The target identifier", example = "ecs1-01")
 					String targetId) throws BusinessException {
 
 		try {
