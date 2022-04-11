efc3c91a300e18a536ae97861dc44b7801f57136
Marc Brasseur <brasseur@sentrysoftware.com>
2022-04-11 09:53:28 -0400

    HWS-424
    Update windows temp path

--- matrix-engine/src/it/java/com/sentrysoftware/matrix/it/SuperConnectorIT.java
+++ matrix-engine/src/it/java/com/sentrysoftware/matrix/it/SuperConnectorIT.java
@@ -16,14 +16,18 @@ import com.sentrysoftware.matrix.it.job.SuperConnectorITJob;
 import com.sentrysoftware.matrix.model.monitoring.HostMonitoring;
 import com.sentrysoftware.matrix.model.monitoring.IHostMonitoring;
 
-import org.apache.tools.ant.util.FileUtils;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.Assert.assertTrue;
+
 import java.io.File;
+import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Stream;
 
 class SuperConnectorIT {
 
@@ -35,8 +39,10 @@ class SuperConnectorIT {
 
 	private static EngineConfiguration engineConfiguration;
 
-	@BeforeAll
-	static void setUp() throws Exception {
+	private static File mshwTmp;
+
+    @BeforeAll
+    static void setUp() throws Exception {
 
 		// Compile the connector and add it to the store
 		ConnectorParser connectorParser = new ConnectorParser();
@@ -50,32 +56,34 @@ class SuperConnectorIT {
 				.target(HardwareTarget.builder().hostname("localhost").id("localhost").type(TargetType.STORAGE).build())
 				.selectedConnectors(Set.of(CONNECTOR_NAME))
 				.protocolConfigurations(Map.of(OSCommandConfig.class, protocol)).build();
-	}
+
+		// Create the MSHW directory under the system's temporary directory
+		// Detect windows
+		if(LocalOSHandler.isWindows()){	
+			mshwTmp = Files.createDirectories(new File(System.getProperty("java.io.tmpdir") + "/MSHW").toPath()).toFile();
+		} else {
+			mshwTmp = Files.createDirectories(new File("/tmp/MSHW/").toPath()).toFile();
+		}
+
+		// Delete existing files, in case a previous execution of this test was brutally stopped
+       	Stream.of(mshwTmp.listFiles())
+                .forEach(file -> assertTrue(file.delete()));
+    }
+
+    @AfterAll
+    static void dispose() {
+        // Make sure the files are removed
+        Stream.of(mshwTmp.listFiles())
+                .forEach(file -> assertTrue(file.delete()));
+        // Delete the MSHW directory
+        mshwTmp.delete();
+    }
 
 	@Test
 	void test() throws Exception {
 
 		final ITJob itJob = new SuperConnectorITJob();
 		final IHostMonitoring hostMonitoring = new HostMonitoring();
-
-
-		File tmp;
-
-		//Remove old test data so that we can run cleanly
-		if(LocalOSHandler.isWindows()){
-			tmp = new File("%TEMP%\\MSHW\\");
-		} else {
-			tmp = new File("/tmp/MSHW/");
-		}
-
-		if(tmp != null && tmp.exists()){
-			for(File f : tmp.listFiles()){
-				FileUtils.delete(f);
-			}
-			tmp.delete();
-		}
-
-		tmp.mkdir();
 			
 		itJob
 			.prepareEngine(engineConfiguration, hostMonitoring)
