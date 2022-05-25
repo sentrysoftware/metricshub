ef0005659b160da4818b2053da17f97579b190c8
Ramassh Theivendran <ramassh@sentrysoftware.com>
2022-05-25 20:24:47 +1200

    HWS-532
    Corrections as per the code review.

--- hws-agent/src/main/java/com/sentrysoftware/hardware/agent/service/opentelemetry/mapping/PowerSupplyMapping.java
+++ hws-agent/src/main/java/com/sentrysoftware/hardware/agent/service/opentelemetry/mapping/PowerSupplyMapping.java
@@ -1,6 +1,9 @@
 package com.sentrysoftware.hardware.agent.service.opentelemetry.mapping;
 
 import static com.sentrysoftware.hardware.agent.service.opentelemetry.mapping.MappingConstants.*;
+import static com.sentrysoftware.matrix.common.helpers.HardwareConstants.ERROR_COUNT_ALARM_THRESHOLD;
+import static com.sentrysoftware.matrix.common.helpers.HardwareConstants.ERROR_COUNT_WARNING_THRESHOLD;
+import static com.sentrysoftware.matrix.common.helpers.HardwareConstants.POWER_SUPPLY_POWER;
 
 import java.util.Collections;
 import java.util.List;
@@ -100,19 +103,30 @@ public class PowerSupplyMapping {
 					.name("hw.power_supply.utilization")
 					.factor(0.01)
 					.unit("1")
-					.description("Power supply utilization.")
+					.description("Ratio of the power supply power currently in use.")
 					.build()
 			)
 		);
 
+		return map;
+	}
+	
+	/**
+	 * Create PowerSupply Metadata to metrics map
+	 * 
+	 * @return {@link Map} of {@link MetricInfo} instances indexed by the matrix parameter names
+	 */
+	static Map<String, List<MetricInfo>> powerSupplyMetadataToMetrics() {
+		final Map<String, List<MetricInfo>> map = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
+
 		map.put(
-			IMetaMonitor.POWER_CONSUMPTION.getName(),
+			POWER_SUPPLY_POWER,
 			Collections.singletonList(
 				MetricInfo
 					.builder()
 					.name("hw.power_supply.power")
 					.unit(WATTS_UNIT)
-					.description("Energy consumed by the power supply.")
+					.description("Maximum power output.")
 					.build()
 			)
 		);
