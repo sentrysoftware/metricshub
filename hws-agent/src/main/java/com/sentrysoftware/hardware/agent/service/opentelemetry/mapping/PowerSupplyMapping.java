39c8f4fe0d4bb249f2caf1e24162a9eb66ad6139
Ramassh Theivendran <ramassh@sentrysoftware.com>
2022-05-25 19:58:37 +1200

    HWS-532
    Normalized Power Supply metrics

---
+++ hws-agent/src/main/java/com/sentrysoftware/hardware/agent/service/opentelemetry/mapping/PowerSupplyMapping.java
@@ -0,0 +1,123 @@
+package com.sentrysoftware.hardware.agent.service.opentelemetry.mapping;
+
+import static com.sentrysoftware.hardware.agent.service.opentelemetry.mapping.MappingConstants.*;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import com.sentrysoftware.hardware.agent.dto.metric.MetricInfo;
+import com.sentrysoftware.hardware.agent.dto.metric.StaticIdentifyingAttribute;
+import com.sentrysoftware.matrix.common.meta.monitor.IMetaMonitor;
+import com.sentrysoftware.matrix.common.meta.monitor.PowerSupply;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class PowerSupplyMapping {
+
+	private static final String POWER_SUPPLY_STATUS_METRIC_NAME = "hw.power_supply.status";
+
+	/**
+	 * Build power supply metrics map
+	 *
+	 * @return  {@link Map} where the metrics are indexed by the matrix parameter name
+	 */
+	static Map<String, List<MetricInfo>> buildPowerSupplyMetricsMapping() {
+		final Map<String, List<MetricInfo>> map = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
+
+		map.put(
+			IMetaMonitor.STATUS.getName(),
+			List.of(
+				MetricInfo
+					.builder()
+					.name(POWER_SUPPLY_STATUS_METRIC_NAME)
+					.description("Whether the power supply status is ok or not.")
+					.identifyingAttribute(
+						StaticIdentifyingAttribute
+							.builder()
+							.key(STATE_ATTRIBUTE_KEY)
+							.value(OK_ATTRIBUTE_VALUE)
+							.build()
+					)
+					.predicate(OK_STATUS_PREDICATE)
+					.build(),
+				MetricInfo
+					.builder()
+					.name(POWER_SUPPLY_STATUS_METRIC_NAME)
+					.description("Whether the power supply status is degraded or not.")
+					.identifyingAttribute(
+						StaticIdentifyingAttribute
+							.builder()
+							.key(STATE_ATTRIBUTE_KEY)
+							.value(DEGRADED_ATTRIBUTE_VALUE)
+							.build()
+					)
+					.predicate(DEGRADED_STATUS_PREDICATE)
+					.build(),
+				MetricInfo
+					.builder()
+					.name(POWER_SUPPLY_STATUS_METRIC_NAME)
+					.description("Whether the power supply status is failed or not.")
+					.identifyingAttribute(
+						StaticIdentifyingAttribute
+							.builder()
+							.key(STATE_ATTRIBUTE_KEY)
+							.value(FAILED_ATTRIBUTE_VALUE)
+							.build()
+					)
+					.predicate(FAILED_STATUS_PREDICATE)
+					.build()
+			)
+		);
+
+		map.put(
+			IMetaMonitor.PRESENT.getName(),
+			Collections.singletonList(
+				MetricInfo
+					.builder()
+					.name(POWER_SUPPLY_STATUS_METRIC_NAME)
+					.description("Whether the power supply is found or not.")
+					.identifyingAttribute(
+						StaticIdentifyingAttribute
+							.builder()
+							.key(STATE_ATTRIBUTE_KEY)
+							.value(PRESENT_ATTRIBUTE_VALUE)
+							.build()
+					)
+					.predicate(PRESENT_PREDICATE)
+					.build()
+			)
+		);
+		
+		map.put(
+			PowerSupply.USED_CAPACITY.getName(),
+			Collections.singletonList(
+				MetricInfo
+					.builder()
+					.name("hw.power_supply.utilization")
+					.factor(0.01)
+					.unit("1")
+					.description("Power supply utilization.")
+					.build()
+			)
+		);
+
+		map.put(
+			IMetaMonitor.POWER_CONSUMPTION.getName(),
+			Collections.singletonList(
+				MetricInfo
+					.builder()
+					.name("hw.power_supply.power")
+					.unit(WATTS_UNIT)
+					.description("Energy consumed by the power supply.")
+					.build()
+			)
+		);
+
+		return map;
+	}
+
+}
