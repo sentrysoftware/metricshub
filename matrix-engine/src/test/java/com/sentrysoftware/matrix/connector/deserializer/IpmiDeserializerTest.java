15edc95380e0ddafddb1962050a2c69ef26860d1
Marc Brasseur <brasseur@sentrysoftware.com>
2023-01-25 11:24:50 -0500

    MTX-24
    Write tests for ipmi criteria parsing

---
+++ matrix-engine/src/test/java/com/sentrysoftware/matrix/connector/deserializer/IpmiDeserializerTest.java
@@ -0,0 +1,38 @@
+package com.sentrysoftware.matrix.connector.deserializer;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import com.sentrysoftware.matrix.connector.model.Connector;
+import com.sentrysoftware.matrix.connector.model.identity.criterion.Criterion;
+import com.sentrysoftware.matrix.connector.model.identity.criterion.Ipmi;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class IpmiDeserializerTest {
+
+  @Test
+  /**
+   * Checks that the criteria type is ipmi
+   *
+   * @throws IOException
+   */
+  void testDeserializeDoesntThrow() throws IOException {
+    final ConnectorDeserializer deserializer = new ConnectorDeserializer();
+    final Connector connector =
+        deserializer.deserialize(new File("src/test/resources/test-files/connector/ipmi.yaml"));
+
+    List<Criterion> expected = new ArrayList<>();
+    expected.add(new Ipmi());
+    
+    assertNotNull(connector);
+    assertEquals("connector", connector.getConnectorIdentity().getCompiledFilename());
+
+    assertNotNull(connector.getConnectorIdentity().getDetection());
+    assertEquals(expected, connector.getConnectorIdentity().getDetection().getCriteria());
+  }
+}
