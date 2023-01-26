b008c5c39e25f3f8d0c276a78578d9a96d6ed1af
Marc Brasseur <brasseur@sentrysoftware.com>
2023-01-26 09:13:17 -0500

    MTX-24
    Update test class name and resource file name for ipmi

--- matrix-engine/src/test/java/com/sentrysoftware/matrix/connector/deserializer/IpmiCriterionDeserializerTest.java
+++ matrix-engine/src/test/java/com/sentrysoftware/matrix/connector/deserializer/IpmiCriterionDeserializerTest.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
-class IpmiDeserializerTest {
+class IpmiCriterionDeserializerTest {
 
   @Test
   /**
@@ -24,7 +24,7 @@ class IpmiDeserializerTest {
   void testDeserializeDoesntThrow() throws IOException {
     final ConnectorDeserializer deserializer = new ConnectorDeserializer();
     final Connector connector =
-        deserializer.deserialize(new File("src/test/resources/test-files/connector/ipmi.yaml"));
+        deserializer.deserialize(new File("src/test/resources/test-files/connector/ipmiCriterion.yaml"));
 
     List<Criterion> expected = new ArrayList<>();
     expected.add(new Ipmi());
