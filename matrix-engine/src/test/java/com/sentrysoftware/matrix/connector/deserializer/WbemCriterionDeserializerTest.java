1a4f997c559e45ed1701aafbe8e9993acab1dfda
Alex <alex@sentrysoftware.com>
2023-01-30 16:58:52 -0500

    MTX-34
    Write tests for wbem parsing

---
+++ matrix-engine/src/test/java/com/sentrysoftware/matrix/connector/deserializer/WbemCriterionDeserializerTest.java
@@ -0,0 +1,68 @@
+package com.sentrysoftware.matrix.connector.deserializer;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import com.sentrysoftware.matrix.connector.model.Connector;
+import com.sentrysoftware.matrix.connector.model.identity.criterion.Criterion;
+import com.sentrysoftware.matrix.connector.model.identity.criterion.Wbem;
+
+class WbemCriterionDeserializerTest {
+
+	@Test
+	/**
+	 * Checks that the criteria type is wbem
+	 *
+	 * @throws IOException
+	 */
+	void testDeserializeWbemCriterion() throws IOException {
+		final ConnectorDeserializer deserializer = new ConnectorDeserializer();
+		final Connector connector = deserializer
+				.deserialize(new File("src/test/resources/test-files/connector/wbemCriterion.yaml"));
+
+		final List<Criterion> expected = new ArrayList<>();
+
+		final Wbem wbem = Wbem.builder()
+				.type("wbem")
+				.query("testQuery")
+				.namespace("testNamespace")
+				.expectedResult("testExpectedResult")
+				.errorMessage("testErrorMessage")
+				.forceSerialization(true)
+				.build();
+
+		expected.add(wbem);
+
+		assertNotNull(connector);
+		assertEquals("wbemCriterion", connector.getConnectorIdentity().getCompiledFilename());
+
+		assertNotNull(connector.getConnectorIdentity().getDetection());
+		List<Criterion> criteria = connector.getConnectorIdentity().getDetection().getCriteria();
+		assertEquals(expected, criteria);
+	}
+
+	@Test
+	/**
+	 * Checks that fields that cannot be null throw an error when they are null
+	 * 
+	 * @throws IOException
+	 */
+	void testDeserializeNonNull() throws IOException {
+		try {
+			final ConnectorDeserializer deserializer = new ConnectorDeserializer();
+			deserializer.deserialize(new File("src/test/resources/test-files/connector/wbemCriterionNonNull.yaml"));
+			Assert.fail();
+		} catch (IllegalArgumentException e) {
+			assertTrue(e.getMessage().contains("cannot be null"));
+		}
+	}
+}
