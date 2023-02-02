5a36d3ffc45bba9eaaf6849c235bf6a0a5d11da6
Alex <alex@sentrysoftware.com>
2023-02-02 14:19:11 -0500

    MTX-35
    Added tests for empty queries and default namespace
    Moved test config files to pre-approved location

--- matrix-engine/src/test/java/com/sentrysoftware/matrix/connector/deserializer/WmiCriterionDeserializerTest.java
+++ matrix-engine/src/test/java/com/sentrysoftware/matrix/connector/deserializer/WmiCriterionDeserializerTest.java
@@ -27,7 +27,7 @@ class WmiCriterionDeserializerTest {
 	void testDeserializeWmiCriterion() throws IOException {
 		final ConnectorDeserializer deserializer = new ConnectorDeserializer();
 		final Connector connector = deserializer
-				.deserialize(new File("src/test/resources/test-files/connector/wmiCriterion.yaml"));
+				.deserialize(new File("src/test/resources/test-files/connector/detection/criteria/wmi/wmiCriterion.yaml"));
 
 		final List<Criterion> expected = new ArrayList<>();
 
@@ -52,17 +52,53 @@ class WmiCriterionDeserializerTest {
 
 	@Test
 	/**
-	 * Checks that fields that cannot be null throw an error when they are null
+	 * Checks that the namespace field gets assigned the proper default value
+	 * 
+	 * @throws IOException
+	 */
+	void testWbemDefaultNamespace() throws IOException {
+		final ConnectorDeserializer deserializer = new ConnectorDeserializer();
+		final Connector connector = deserializer
+				.deserialize(new File("src/test/resources/test-files/connector/detection/criteria/wmi/wmiCriterionDefaultNamespace.yaml"));
+
+		final List<Criterion> expected = new ArrayList<>();
+
+		final Wmi wmi = Wmi.builder()
+				.type("wbem")
+				.build();
+
+		expected.add(wmi);
+		
+		assertNotNull(connector.getConnectorIdentity().getDetection());
+		List<Criterion> criteria = connector.getConnectorIdentity().getDetection().getCriteria();
+		assertEquals(expected, criteria);
+	}
+
+	@Test
+	/**
+	 * Checks that the query field throws an error when they are null or empty
 	 * 
 	 * @throws IOException
 	 */
 	void testWmiNonNull() throws IOException {
-		try {
-			final ConnectorDeserializer deserializer = new ConnectorDeserializer();
-			deserializer.deserialize(new File("src/test/resources/test-files/connector/wmiCriterionNonNull.yaml"));
-			Assert.fail();
-		} catch (IllegalArgumentException e) {
-			assertTrue(e.getMessage().contains("cannot be null"));
+		{
+			try {
+				final ConnectorDeserializer deserializer = new ConnectorDeserializer();
+				deserializer.deserialize(new File("src/test/resources/test-files/connector/wmi/wmiCriterionNullQuery.yaml"));
+				Assert.fail();
+			} catch (IllegalArgumentException e) {
+				assertTrue(e.getMessage().contains("Query cannot be null."));
+			}
+		}
+
+		{
+			try {
+				final ConnectorDeserializer deserializer = new ConnectorDeserializer();
+				deserializer.deserialize(new File("src/test/resources/test-files/connector/wmi/wmiCriterionEmptyQuery.yaml"));
+				Assert.fail();
+			} catch (IllegalArgumentException e) {
+				assertTrue(e.getMessage().contains("Query cannot be empty."));
+			}
 		}
 	}
 }
