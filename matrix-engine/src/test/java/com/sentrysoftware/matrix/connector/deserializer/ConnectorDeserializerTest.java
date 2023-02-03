3c167cd5ed39018704ed84347a39f978008bb144
Marc Brasseur <brasseur@sentrysoftware.com>
2023-02-03 10:17:57 -0500

    MTX-48
    create abstract class and interface to simplify tests

--- matrix-engine/src/test/java/com/sentrysoftware/matrix/connector/deserializer/ConnectorDeserializerTest.java
+++ matrix-engine/src/test/java/com/sentrysoftware/matrix/connector/deserializer/ConnectorDeserializerTest.java
@@ -3,7 +3,6 @@ package com.sentrysoftware.matrix.connector.deserializer;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import java.io.File;
 import java.io.IOException;
 
 import org.junit.jupiter.api.Test;
@@ -11,14 +10,16 @@ import org.junit.jupiter.api.Test;
 import com.sentrysoftware.matrix.connector.model.Connector;
 import com.sentrysoftware.matrix.connector.model.identity.ConnectorIdentity;
 
-class ConnectorDeserializerTest {
+class ConnectorDeserializerTest extends DeserializerTest {
 
+	@Override
+	public String getResourcePath() {
+		return "src/test/resources/test-files/connector/";
+	}
+	
 	@Test
 	void testDeserializeConnector() throws IOException {
-		final ConnectorDeserializer deserializer = new ConnectorDeserializer();
-		final Connector connector = deserializer.deserialize(
-			new File("src/test/resources/test-files/connector/connector.yaml")
-		);
+		final Connector connector = getConnector("connector");
 		final ConnectorIdentity connectorIdentity = connector.getConnectorIdentity();
 
 		assertNotNull(connector);
