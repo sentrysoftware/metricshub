1e52d06177cf1ab6ac828f4a00987acc525d11e5
Nassim Boutekedjiret <nassim@sentrysoftware.com>
2023-02-06 10:32:31 +0100

    MTX-50
    * Updated code to handle null value
    * Formatted TimeoutDeserializer code (tabs instead of whitespace)

--- matrix-engine/src/main/java/com/sentrysoftware/matrix/connector/deserializer/custom/TimeoutDeserializer.java
+++ matrix-engine/src/main/java/com/sentrysoftware/matrix/connector/deserializer/custom/TimeoutDeserializer.java
@@ -8,22 +8,23 @@ import java.io.IOException;
 
 public class TimeoutDeserializer extends JsonDeserializer<Long> {
 
-    @Override
-    public Long deserialize(JsonParser parser, DeserializationContext ctxt) throws IOException {
-        if (parser == null)
-            return null;
+	@Override
+	public Long deserialize(JsonParser parser, DeserializationContext ctxt) throws IOException {
+		if (parser == null)
+			return null;
 
-        final String key = parser.getCurrentName();
-        final Long value = parser.getValueAsLong();
+		final String key = parser.getCurrentName();
+		final Long value = parser.getValueAsLong();
 
-        if (value <= 0) {
-            throw new InvalidFormatException(
-                parser,
-                String.format("Invalid negative or zero value encountered for property '%s'.", key),
-                value,
-                String.class);
-        } else {
-            return value;
-        }
-    }
+		if (value == null || value > 0) {
+			return value;
+		}
+
+		throw new InvalidFormatException(
+			parser,
+			String.format("Invalid negative or zero value encountered for property '%s'.", key),
+			value,
+			String.class
+		);
+	}
 }
