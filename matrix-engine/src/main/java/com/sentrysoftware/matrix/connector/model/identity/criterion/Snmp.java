8b938f66bc9d549497379b812921aa3f67bd7363
Marc Brasseur <brasseur@sentrysoftware.com>
2023-02-06 12:10:47 -0500

    MTX-31
    write tests for snmpGetNext criteria parsing

--- matrix-engine/src/main/java/com/sentrysoftware/matrix/connector/model/identity/criterion/Snmp.java
+++ matrix-engine/src/main/java/com/sentrysoftware/matrix/connector/model/identity/criterion/Snmp.java
@@ -1,8 +1,17 @@
 package com.sentrysoftware.matrix.connector.model.identity.criterion;
 
+import com.fasterxml.jackson.annotation.JsonSetter;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.sentrysoftware.matrix.connector.deserializer.custom.NonBlankDeserializer;
+
+import static com.fasterxml.jackson.annotation.Nulls.FAIL;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 import lombok.ToString;
 
 @Data
@@ -13,10 +22,17 @@ public abstract class Snmp extends Criterion {
 
 	private static final long serialVersionUID = 1L;
 
+	@NonNull
+	@JsonSetter(nulls = FAIL)
+	@JsonDeserialize(using = NonBlankDeserializer.class)
 	private String oid;
 	private String expectedResult;
 
-	protected Snmp(String type, boolean forceSerialization, String oid, String expectedResult) {
+	protected Snmp(
+			@JsonProperty("type") String type, 
+			@JsonProperty("forceSerialization") boolean forceSerialization, 
+			@JsonProperty(value = "oid", required = true) @NonNull String oid, 
+			@JsonProperty("expectedResult") String expectedResult) {
 
 		super(type, forceSerialization);
 		this.oid = oid;
