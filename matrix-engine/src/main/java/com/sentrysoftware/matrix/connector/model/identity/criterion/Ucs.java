2698baf4462d1f4aae71e7bbcb06b68728890ae5
Marc Brasseur <brasseur@sentrysoftware.com>
2023-02-06 10:28:33 -0500

    MTX-33
    Write tests for ucs criteria parsing

--- matrix-engine/src/main/java/com/sentrysoftware/matrix/connector/model/identity/criterion/Ucs.java
+++ matrix-engine/src/main/java/com/sentrysoftware/matrix/connector/model/identity/criterion/Ucs.java
@@ -1,9 +1,17 @@
 package com.sentrysoftware.matrix.connector.model.identity.criterion;
 
+import static com.fasterxml.jackson.annotation.Nulls.FAIL;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSetter;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.sentrysoftware.matrix.connector.deserializer.custom.NonBlankDeserializer;
+
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 import lombok.ToString;
 
 @Data
@@ -14,17 +22,24 @@ public class Ucs extends Criterion {
 
 	private static final long serialVersionUID = 1L;
 
+	@NonNull
+	@JsonSetter(nulls = FAIL)
+	@JsonDeserialize(using = NonBlankDeserializer.class)
 	private String query;
 	private String errorMessage;
 	private String expectedResult;
 
 	@Builder
-	public Ucs(String type, boolean forceSerialization, String query, String errorMessage, String expectedResult) {
+	public Ucs(
+			@JsonProperty("type") String type,
+			@JsonProperty("forceSerialization") boolean forceSerialization,
+			@JsonProperty(value = "query", required = true) @NonNull String query,
+			@JsonProperty("errorMessage") String errorMessage,
+			@JsonProperty("expectedResult") String expectedResult) {
 
 		super(type, forceSerialization);
 		this.query = query;
 		this.errorMessage = errorMessage;
 		this.expectedResult = expectedResult;
 	}
-
 }
