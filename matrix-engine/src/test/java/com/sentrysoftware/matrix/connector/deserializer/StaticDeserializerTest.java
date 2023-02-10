8278742bf096cfc28fc49beeeb1026d2b6108499
Marc Brasseur <brasseur@sentrysoftware.com>
2023-02-10 14:45:29 -0500

    MTX-65
    test static source parsing

---
+++ matrix-engine/src/test/java/com/sentrysoftware/matrix/connector/deserializer/StaticDeserializerTest.java
@@ -0,0 +1,37 @@
+package com.sentrysoftware.matrix.connector.deserializer;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+import com.sentrysoftware.matrix.connector.model.Connector;
+import com.sentrysoftware.matrix.connector.model.monitor.task.source.Source;
+import com.sentrysoftware.matrix.connector.model.monitor.task.source.StaticSource;
+
+class StaticDeserializerTest extends DeserializerTest {
+
+	@Override
+	public String getResourcePath() {
+		return "src/test/resources/test-files/source/static/";
+	}
+
+	@Test
+	void testDeserializeStatic() throws IOException {
+		final String testResource = "static";
+		final Connector connector = getConnector(testResource);
+
+		final Map<String, Source> expected = new LinkedHashMap<String, Source>(
+			Map.of("testStaticSource", 
+					StaticSource.builder()
+							.key("$pre.testStaticSource")
+							.type("static")
+							.value("testValue")
+							.build()));
+
+		assertEquals(expected, connector.getPre());
+	}
+}
