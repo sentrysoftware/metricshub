776573e2dbf80e9a6de0adf5b06e142f0ee38891
Marc Brasseur <brasseur@sentrysoftware.com>
2023-02-10 15:16:01 -0500

    MTX-66
    test ipmi source parsing

---
+++ matrix-engine/src/test/java/com/sentrysoftware/matrix/connector/deserializer/IpmiDeserializerTest.java
@@ -0,0 +1,36 @@
+package com.sentrysoftware.matrix.connector.deserializer;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+import com.sentrysoftware.matrix.connector.model.Connector;
+import com.sentrysoftware.matrix.connector.model.monitor.task.source.IpmiSource;
+import com.sentrysoftware.matrix.connector.model.monitor.task.source.Source;
+
+class IpmiDeserializerTest extends DeserializerTest {
+
+	@Override
+	public String getResourcePath() {
+		return "src/test/resources/test-files/source/ipmi/";
+	}
+
+	@Test
+	void testDeserializeStatic() throws IOException {
+		final String testResource = "ipmi";
+		final Connector connector = getConnector(testResource);
+
+		final Map<String, Source> expected = new LinkedHashMap<String, Source>(
+			Map.of("testIpmiSource", 
+					IpmiSource.builder()
+							.key("$pre.testIpmiSource")
+							.type("ipmi")
+							.build()));
+
+		assertEquals(expected, connector.getPre());
+	}
+}
