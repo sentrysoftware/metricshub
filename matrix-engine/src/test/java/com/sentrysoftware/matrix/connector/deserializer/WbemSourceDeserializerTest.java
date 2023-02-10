a05570e87914664486c3fbfe2b1b9402d4fbfadd
Marc Brasseur <brasseur@sentrysoftware.com>
2023-02-10 14:24:27 -0500

    MTX-58
    test wbem source parsing

---
+++ matrix-engine/src/test/java/com/sentrysoftware/matrix/connector/deserializer/WbemSourceDeserializerTest.java
@@ -0,0 +1,37 @@
+package com.sentrysoftware.matrix.connector.deserializer;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+import com.sentrysoftware.matrix.connector.model.Connector;
+import com.sentrysoftware.matrix.connector.model.monitor.task.source.Source;
+import com.sentrysoftware.matrix.connector.model.monitor.task.source.WbemSource;
+
+class WbemSourceDeserializerTest extends DeserializerTest {
+
+	@Override
+	public String getResourcePath() {
+		return "src/test/resources/test-files/source/wbem/";
+	}
+
+	@Test
+	void testDeserializeWbemSource() throws IOException {
+		final String testResource = "wbem";
+		final Connector connector = getConnector(testResource);
+
+		final Map<String, Source> expected = new LinkedHashMap<String, Source>(
+				Map.of("testWbemSource",
+						WbemSource.builder()
+								.key("$pre.testWbemSource")
+								.type("wbem")
+								.query("testQuery")
+								.namespace("testNamespace")
+								.build()));
+
+		assertEquals(expected, connector.getPre());
+	}
+}
