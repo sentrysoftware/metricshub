158cf322e129d1e01db23e543a2920059e1440fb
Alex <alex@sentrysoftware.com>
2023-02-10 23:16:04 -0500

    MTX-70
    Wrote tests for UcsSource

---
+++ matrix-engine/src/test/java/com/sentrysoftware/matrix/connector/deserializer/source/ucs/UcsDeserializerTest.java
@@ -0,0 +1,53 @@
+package com.sentrysoftware.matrix.connector.deserializer.source.ucs;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+import com.sentrysoftware.matrix.connector.deserializer.DeserializerTest;
+import com.sentrysoftware.matrix.connector.model.Connector;
+import com.sentrysoftware.matrix.connector.model.monitor.task.source.Source;
+import com.sentrysoftware.matrix.connector.model.monitor.task.source.UcsSource;
+
+public class UcsDeserializerTest extends DeserializerTest{
+	@Override
+	public String getResourcePath() {
+		return "src/test/resources/test-files/source/ucs/";
+	}
+
+	@Test
+	void testDeserializeTableJoin() throws IOException {
+		final String testResource = "ucs";
+
+		List<String> queries = new ArrayList<>();
+		queries.add("testQuery1");
+		queries.add("testQuery2");
+		queries.add("testQuery3");
+
+		List<String> columns = new ArrayList<>();
+		columns.add("1");
+		columns.add("2");
+		columns.add("3");
+		
+
+		final Connector connector = getConnector(testResource);
+		Map<String, Source> expected = new LinkedHashMap<>();
+		expected.put("testUcsSource",
+				UcsSource.builder()
+						.key("$pre.testUcsSource")
+						.type("ucs")
+						.queries(queries)
+						.exclude("testExclude")
+						.keep("testKeep")
+						.selectColumns(columns)
+						.build());
+
+		assertEquals(expected, connector.getPre());
+	}
+}
