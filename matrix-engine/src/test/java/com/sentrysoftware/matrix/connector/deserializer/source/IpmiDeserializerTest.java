73a6782b6ad1ba735ce405d5d9877107c7128609
Marc Brasseur <brasseur@sentrysoftware.com>
2023-02-14 09:11:01 -0500

    MTX-66
    move class to correct package

--- matrix-engine/src/test/java/com/sentrysoftware/matrix/connector/deserializer/source/IpmiDeserializerTest.java
+++ matrix-engine/src/test/java/com/sentrysoftware/matrix/connector/deserializer/source/IpmiDeserializerTest.java
@@ -1,4 +1,4 @@
-package com.sentrysoftware.matrix.connector.deserializer;
+package com.sentrysoftware.matrix.connector.deserializer.source;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -8,6 +8,7 @@ import java.util.Map;
 
 import org.junit.jupiter.api.Test;
 
+import com.sentrysoftware.matrix.connector.deserializer.DeserializerTest;
 import com.sentrysoftware.matrix.connector.model.Connector;
 import com.sentrysoftware.matrix.connector.model.monitor.task.source.IpmiSource;
 import com.sentrysoftware.matrix.connector.model.monitor.task.source.Source;
