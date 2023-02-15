c0d649146ae4d791c2d48badf9a933bdf416f977
Nassim Boutekedjiret <nassim@sentrysoftware.com>
2023-02-15 10:19:28 +0100

    MTX-79
    * StaticSource.value is now required.

--- matrix-engine/src/main/java/com/sentrysoftware/matrix/connector/model/monitor/task/source/StaticSource.java
+++ matrix-engine/src/main/java/com/sentrysoftware/matrix/connector/model/monitor/task/source/StaticSource.java
@@ -1,5 +1,6 @@
 package com.sentrysoftware.matrix.connector.model.monitor.task.source;
 
+import static com.fasterxml.jackson.annotation.Nulls.FAIL;
 import static com.sentrysoftware.matrix.common.helpers.MatrixConstants.NEW_LINE;
 import static com.sentrysoftware.matrix.common.helpers.StringHelper.addNonNull;
 
@@ -8,7 +9,9 @@ import java.util.List;
 import java.util.StringJoiner;
 import java.util.function.UnaryOperator;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSetter;
 import com.sentrysoftware.matrix.connector.model.common.ExecuteForEachEntryOf;
 import com.sentrysoftware.matrix.connector.model.monitor.task.source.compute.Compute;
 
@@ -16,6 +19,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 @Data
 @NoArgsConstructor
@@ -24,14 +28,17 @@ public class StaticSource  extends Source {
 
 	private static final long serialVersionUID = 1L;
 
+	@NonNull
+	@JsonSetter(nulls = FAIL)
 	private String value;
 
 	@Builder
+	@JsonCreator
 	public StaticSource(
 			@JsonProperty("type") String type, 
 			@JsonProperty("computes") List<Compute> computes,
 			@JsonProperty("forceSerialization") boolean forceSerialization,
-			@JsonProperty("value") String value,
+			@JsonProperty(value = "value", required = true) @NonNull String value,
 			@JsonProperty("key") String key,
 			@JsonProperty("executeForEachEntryOf") ExecuteForEachEntryOf executeForEachEntryOf
 	) {
