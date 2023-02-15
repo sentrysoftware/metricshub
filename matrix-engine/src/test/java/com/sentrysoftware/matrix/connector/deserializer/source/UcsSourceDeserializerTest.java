f50811c27520f6cb24942fdaf34945c0bbb871f4
Nassim Boutekedjiret <nassim@sentrysoftware.com>
2023-02-15 11:04:27 +0100

    MTX-70
    * Updated code to make the set of queries consistent.

--- matrix-engine/src/test/java/com/sentrysoftware/matrix/connector/deserializer/source/UcsSourceDeserializerTest.java
+++ matrix-engine/src/test/java/com/sentrysoftware/matrix/connector/deserializer/source/UcsSourceDeserializerTest.java
@@ -1,12 +1,12 @@
-package com.sentrysoftware.matrix.connector.deserializer.source.ucs;
+package com.sentrysoftware.matrix.connector.deserializer.source;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.LinkedHashMap;
-import java.util.List;
+import java.util.LinkedHashSet;
 import java.util.Map;
+import java.util.Set;
 
 import org.junit.jupiter.api.Test;
 
@@ -15,7 +15,7 @@ import com.sentrysoftware.matrix.connector.model.Connector;
 import com.sentrysoftware.matrix.connector.model.monitor.task.source.Source;
 import com.sentrysoftware.matrix.connector.model.monitor.task.source.UcsSource;
 
-public class UcsDeserializerTest extends DeserializerTest{
+class UcsSourceDeserializerTest extends DeserializerTest{
 	@Override
 	public String getResourcePath() {
 		return "src/test/resources/test-files/source/ucs/";
@@ -25,28 +25,22 @@ public class UcsDeserializerTest extends DeserializerTest{
 	void testDeserializeUcs() throws IOException {
 		final String testResource = "ucs";
 
-		List<String> queries = new ArrayList<>();
-		queries.add("testQuery1");
-		queries.add("testQuery2");
-		queries.add("testQuery3");
-
-		List<String> columns = new ArrayList<>();
-		columns.add("1");
-		columns.add("2");
-		columns.add("3");
-		
+		final Set<String> queries = new LinkedHashSet<>(
+			Set.of("testQuery1", "testQuery2", "testQuery3")
+		);
 
 		final Connector connector = getConnector(testResource);
 		Map<String, Source> expected = new LinkedHashMap<>();
 		expected.put("testUcsSource",
-				UcsSource.builder()
-						.key("$pre.testUcsSource")
-						.type("ucs")
-						.queries(queries)
-						.exclude("testExclude")
-						.keep("testKeep")
-						.selectColumns(columns)
-						.build());
+			UcsSource.builder()
+				.key("$pre.testUcsSource")
+				.type("ucs")
+				.queries(queries)
+				.exclude("testExclude")
+				.keep("testKeep")
+				.selectColumns("1,2,3")
+				.build()
+		);
 
 		assertEquals(expected, connector.getPre());
 	}
