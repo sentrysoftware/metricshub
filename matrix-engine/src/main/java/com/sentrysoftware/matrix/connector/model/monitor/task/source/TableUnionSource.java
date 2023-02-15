1d6ae822a139f730752db5e6100222879ae8db30
Marc Brasseur <brasseur@sentrysoftware.com>
2023-02-15 09:35:34 -0500

    MTX-77
    write code for tableunion source parsing

--- matrix-engine/src/main/java/com/sentrysoftware/matrix/connector/model/monitor/task/source/TableUnionSource.java
+++ matrix-engine/src/main/java/com/sentrysoftware/matrix/connector/model/monitor/task/source/TableUnionSource.java
@@ -1,5 +1,7 @@
 package com.sentrysoftware.matrix.connector.model.monitor.task.source;
 
+import static com.fasterxml.jackson.annotation.Nulls.FAIL;
+
 import static com.sentrysoftware.matrix.common.helpers.MatrixConstants.NEW_LINE;
 import static com.sentrysoftware.matrix.common.helpers.StringHelper.addNonNull;
 
@@ -8,6 +10,10 @@ import java.util.List;
 import java.util.StringJoiner;
 import java.util.function.UnaryOperator;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSetter;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.sentrysoftware.matrix.connector.deserializer.custom.TablesDeserializer;
 import com.sentrysoftware.matrix.connector.model.common.ExecuteForEachEntryOf;
 import com.sentrysoftware.matrix.connector.model.monitor.task.source.compute.Compute;
 
@@ -15,6 +21,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 @Data
 @NoArgsConstructor
@@ -23,16 +30,19 @@ public class TableUnionSource extends Source {
 
 	private static final long serialVersionUID = 1L;
 
+	@NonNull
+	@JsonSetter(nulls = FAIL)
+	@JsonDeserialize(using = TablesDeserializer.class)
 	private List<String> tables = new ArrayList<>();
 
 	@Builder
 	public TableUnionSource(
-		String type,
-		List<Compute> computes,
-		boolean forceSerialization,
-		List<String> tables,
-		String key,
-		ExecuteForEachEntryOf executeForEachEntryOf) {
+		@JsonProperty("type") String type,
+		@JsonProperty("computes") List<Compute> computes,
+		@JsonProperty("forceSerialization") boolean forceSerialization,
+		@JsonProperty(value = "tables", required = true) @NonNull List<String> tables,
+		@JsonProperty("key") String key,
+		@JsonProperty("executeForEachEntryOf") ExecuteForEachEntryOf executeForEachEntryOf) {
 
 		super(type, computes, forceSerialization, key, executeForEachEntryOf);
 		this.tables = tables;
