1f9eb96e0d752b398517fc33db599758cc6e1c51
Nassim Boutekedjiret <nassim@sentrysoftware.com>
2023-02-28 20:57:01 +0100

    MTX-63
    * Added simple unit tests (OneExtends)
    * Added depth extends test.

--- matrix-engine/src/test/java/com/sentrysoftware/matrix/connector/parser/ConnectorParserTest.java
+++ matrix-engine/src/test/java/com/sentrysoftware/matrix/connector/parser/ConnectorParserTest.java
@@ -7,8 +7,18 @@ import org.junit.jupiter.api.Test;
 class ConnectorParserTest {
 
 	@Test
+	void testExtendsManagementArrayObjectsDepthExtends() throws IOException {
+		new ConnectorParserExtendsManagement("arrayObjectsDepthExtends").test();
+	}
+
+	@Test
 	void testExtendsManagementArrayObjectsMerge() throws IOException {
 		new ConnectorParserExtendsManagement("arrayObjectsMerge").test();
+	}
+
+	@Test
+	void testExtendsManagementArrayObjectsMergeOneExtends() throws IOException {
+		new ConnectorParserExtendsManagement("arrayObjectsMergeOneExtends").test();
 	}
 
 	@Test
@@ -17,8 +27,18 @@ class ConnectorParserTest {
 	}
 
 	@Test
+	void testExtendsManagementMergeObjectsOneExtends() throws IOException {
+		new ConnectorParserExtendsManagement("mergeObjectsOneExtends").test();
+	}
+
+	@Test
 	void testExtendsManagementOverwriteArraysSimpleValues() throws IOException {
 		new ConnectorParserExtendsManagement("overwriteArraysSimpleValues").test();
+	}
+
+	@Test
+	void testExtendsManagementOverwriteArraysSimpleValuesOneExtends() throws IOException {
+		new ConnectorParserExtendsManagement("overwriteArraysSimpleValuesOneExtends").test();
 	}
 
 }
