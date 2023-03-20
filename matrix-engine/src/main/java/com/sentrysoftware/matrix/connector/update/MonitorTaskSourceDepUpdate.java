0674876139395ece63fa659039c8040b1d95521f
Nassim Boutekedjiret <nassim@sentrysoftware.com>
2023-03-20 11:42:33 +0100

    MTX-117
    * Removed yaml objects `multiCollect` and `monoCollect`.
    
    * Created new yaml object `collect` with `type` property which can be
    `multiInstance` or `monoInstance`.
    
    * Removed `CollectDeserializer`.
    
    * Updated unit tests.

--- matrix-engine/src/main/java/com/sentrysoftware/matrix/connector/update/MonitorTaskSourceDepUpdate.java
+++ matrix-engine/src/main/java/com/sentrysoftware/matrix/connector/update/MonitorTaskSourceDepUpdate.java
@@ -7,10 +7,9 @@ import com.sentrysoftware.matrix.connector.model.Connector;
 import com.sentrysoftware.matrix.connector.model.monitor.AllAtOnceMonitorJob;
 import com.sentrysoftware.matrix.connector.model.monitor.MonitorJob;
 import com.sentrysoftware.matrix.connector.model.monitor.StandardMonitorJob;
+import com.sentrysoftware.matrix.connector.model.monitor.task.AbstractCollect;
 import com.sentrysoftware.matrix.connector.model.monitor.task.AllAtOnce;
 import com.sentrysoftware.matrix.connector.model.monitor.task.Discovery;
-import com.sentrysoftware.matrix.connector.model.monitor.task.MonoCollect;
-import com.sentrysoftware.matrix.connector.model.monitor.task.MultiCollect;
 import com.sentrysoftware.matrix.connector.model.monitor.task.source.Source;
 
 public class MonitorTaskSourceDepUpdate extends SourceConnectorUpdateChain {
@@ -22,8 +21,8 @@ public class MonitorTaskSourceDepUpdate extends SourceConnectorUpdateChain {
 			final String jobName = entry.getKey();
 			final MonitorJob job = entry.getValue();
 
-			if (job instanceof StandardMonitorJob standardMonitor) {
-				final Discovery discovery = standardMonitor.getDiscovery();
+			if (job instanceof StandardMonitorJob standardMonitorJob) {
+				final Discovery discovery = standardMonitorJob.getDiscovery();
 				final Map<String, Source> sources = discovery.getSources();
 				discovery.setSourceDep(
 					updateSourceDependency(
@@ -40,41 +39,23 @@ public class MonitorTaskSourceDepUpdate extends SourceConnectorUpdateChain {
 					)
 				);
 
-				if (standardMonitor.getCollect() instanceof MonoCollect monoCollect) {
-					final Map<String, Source> monoCollectSources = monoCollect.getSources();
-					monoCollect.setSourceDep(
-						updateSourceDependency(
-							monoCollectSources,
-							Pattern.compile(
-								String.format(
-									"\\s*(\\$((?i)monitors)\\.%s\\.((?i)monocollect\\.sources)\\.(%s)\\$)\\s*",
-									Pattern.quote(jobName),
-									getSourceIdentifiersRegex(monoCollectSources)
-								),
-								Pattern.MULTILINE
-							),
-							4
-						)
-					);
-				}
 
-				if (standardMonitor.getCollect() instanceof MultiCollect multiCollect) {
-					final Map<String, Source> multiCollectSources = multiCollect.getSources();
-					multiCollect.setSourceDep(
-						updateSourceDependency(
-							multiCollectSources,
-							Pattern.compile(
-								String.format(
-									"\\s*(\\$((?i)monitors)\\.%s\\.((?i)multicollect\\.sources)\\.(%s)\\$)\\s*",
-									Pattern.quote(jobName),
-									getSourceIdentifiersRegex(multiCollectSources)
-								),
-								Pattern.MULTILINE
+				final AbstractCollect collect = standardMonitorJob.getCollect();
+				final Map<String, Source> collectSources = collect.getSources();
+				collect.setSourceDep(
+					updateSourceDependency(
+						collectSources,
+						Pattern.compile(
+							String.format(
+								"\\s*(\\$((?i)monitors)\\.%s\\.((?i)collect\\.sources)\\.(%s)\\$)\\s*",
+								Pattern.quote(jobName),
+								getSourceIdentifiersRegex(collectSources)
 							),
-							4
-						)
-					);
-				}
+							Pattern.MULTILINE
+						),
+						4
+					)
+				);
 			}
 
 			if (job instanceof AllAtOnceMonitorJob allAtOnceMonitor) {
