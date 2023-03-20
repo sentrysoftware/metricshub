d68923bbce294d05513a479609aafd1426388931
Melinda <melinda@sentrysoftware.com>
2023-03-20 12:13:54 -0400

    MTX-125
    test snmp get source conversion

---
+++ hdf-to-yaml-converter/src/test/java/com/sentrysoftware/matrix/converter/state/source/SnmpSourceConverterTest.java
@@ -0,0 +1,28 @@
+package com.sentrysoftware.matrix.converter.state.source;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import com.sentrysoftware.matrix.converter.AbstractConnectorPropertyConverterTest;
+
+class SnmpSourceConverterTest extends AbstractConnectorPropertyConverterTest {
+
+    @Override
+    protected String getResourcePath() {
+        return "src/test/resources/test-files/monitors/source/snmp";
+    }
+
+    @Test
+    @Disabled("until SNMP Source coverter is up")
+    void test() throws IOException {
+        testConversion("discoveryTable");
+        testConversion("collectTable");
+
+        testConversion("discoveryGet");
+        testConversion("collectGet");
+
+        testAll();
+    }
+}
