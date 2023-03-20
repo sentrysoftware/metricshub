f96c247766f5928dfc9d521919339e11cf3f83fb
Melinda <melinda@sentrysoftware.com>
2023-03-20 12:57:57 -0400

    MTX-129
    test copy sourcer conversion

---
+++ hdf-to-yaml-converter/src/test/java/com/sentrysoftware/matrix/converter/state/source/CopySourceConverterTest.java
@@ -0,0 +1,27 @@
+package com.sentrysoftware.matrix.converter.state.source;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import com.sentrysoftware.matrix.converter.AbstractConnectorPropertyConverterTest;
+
+class CopySourceConverterTest extends AbstractConnectorPropertyConverterTest {
+
+    @Override
+    protected String getResourcePath() {
+        return "src/test/resources/test-files/monitors/source/copy";
+    }
+
+    @Test
+    @Disabled("until Copy Source coverter is up")
+    void test() throws IOException {
+        testConversion("discovery");
+        testConversion("collect");
+
+        testConversion("discoveryFromCollect");
+
+        testAll();
+    }
+}
