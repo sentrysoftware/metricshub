7cf57c22d4c0d6033c2f438e8c92cd4e9f66a577
Melinda <melinda@sentrysoftware.com>
2023-03-22 09:19:36 -0400

    MTX-140
    develop copy source and static source conversion

--- hdf-to-yaml-converter/src/test/java/com/sentrysoftware/matrix/converter/state/source/CopySourceConverterTest.java
+++ hdf-to-yaml-converter/src/test/java/com/sentrysoftware/matrix/converter/state/source/CopySourceConverterTest.java
@@ -2,7 +2,7 @@ package com.sentrysoftware.matrix.converter.state.source;
 
 import java.io.IOException;
 
-import org.junit.jupiter.api.Disabled;
+
 import org.junit.jupiter.api.Test;
 
 import com.sentrysoftware.matrix.converter.AbstractConnectorPropertyConverterTest;
@@ -15,7 +15,6 @@ class CopySourceConverterTest extends AbstractConnectorPropertyConverterTest {
 	}
 
 	@Test
-	@Disabled("until Copy Source converter is up")
 	void test() throws IOException {
 		testConversion("discovery");
 		testConversion("collect");
