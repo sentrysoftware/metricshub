213420aec1fd436e4c4e7c92497f62a95de5d3dc
Nassim Boutekedjiret <nassim@sentrysoftware.com>
2023-03-24 16:19:59 +0100

    MTX-156
    * Code formatting before merge.

--- hdf-to-yaml-converter/src/main/java/com/sentrysoftware/matrix/converter/state/computes/common/StringProcessor.java
+++ hdf-to-yaml-converter/src/main/java/com/sentrysoftware/matrix/converter/state/computes/common/StringProcessor.java
@@ -11,8 +11,9 @@ import com.sentrysoftware.matrix.converter.state.ConversionHelper;
 public class StringProcessor extends AbstractStateConverter {
 
 	private static final Pattern PATTERN = Pattern.compile(
-			ConversionHelper.buildComputeKeyRegex("string"),
-			Pattern.CASE_INSENSITIVE);
+		ConversionHelper.buildComputeKeyRegex("string"),
+		Pattern.CASE_INSENSITIVE
+	);
 
 	@Override
 	public void convert(String key, String value, JsonNode connector, PreConnector preConnector) {
