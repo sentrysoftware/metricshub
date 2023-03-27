baef3a8d7477b47c0190a114733944babd663ca7
Fadhela Kerdjoudj <fadhela@sentrysoftware.com>
2023-03-27 11:21:23 +0200

    MTX-152
    Develop ExtractPropertyFromWBEMPath compute operation

--- hdf-to-yaml-converter/src/main/java/com/sentrysoftware/matrix/converter/state/computes/extractpropertyfromwbempath/ConnectorExtractPropertyFromWbemPathProperty.java
+++ hdf-to-yaml-converter/src/main/java/com/sentrysoftware/matrix/converter/state/computes/extractpropertyfromwbempath/ConnectorExtractPropertyFromWbemPathProperty.java
@@ -1,9 +1,6 @@
 package com.sentrysoftware.matrix.converter.state.computes.extractpropertyfromwbempath;
 
-import java.util.Collections;
 import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import com.sentrysoftware.matrix.converter.state.IConnectorStateConverter;
 import com.sentrysoftware.matrix.converter.state.computes.common.ColumnProcessor;
@@ -15,10 +12,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ConnectorExtractPropertyFromWbemPathProperty {
 
-	private static final String HDF_TYPE_VALUE = "";
-	private static final String YAML_TYPE_VALUE = "";
+	private static final String HDF_TYPE_VALUE = "ExtractPropertyFromWbemPath";
+	private static final String YAML_TYPE_VALUE = "extractPropertyFromWbemPath";
 
 	public static Set<IConnectorStateConverter> getConnectorProperties() {
-		return Collections.emptySet();
+		return Set.of(
+				new ComputeTypeProcessor(HDF_TYPE_VALUE, YAML_TYPE_VALUE),
+				new ColumnProcessor(),
+				new PropertyProcessor()
+			);
 	}
 }
