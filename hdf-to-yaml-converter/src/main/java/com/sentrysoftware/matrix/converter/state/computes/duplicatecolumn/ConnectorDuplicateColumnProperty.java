579de40dce8cdea3342a7dc78bf607904a4e057d
Nassim Boutekedjiret <nassim@sentrysoftware.com>
2023-03-27 10:34:35 +0200

    MTX-149
    * Small formatting before merge

--- hdf-to-yaml-converter/src/main/java/com/sentrysoftware/matrix/converter/state/computes/duplicatecolumn/ConnectorDuplicateColumnProperty.java
+++ hdf-to-yaml-converter/src/main/java/com/sentrysoftware/matrix/converter/state/computes/duplicatecolumn/ConnectorDuplicateColumnProperty.java
@@ -1,8 +1,6 @@
 package com.sentrysoftware.matrix.converter.state.computes.duplicatecolumn;
 
 import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import com.sentrysoftware.matrix.converter.state.IConnectorStateConverter;
 import com.sentrysoftware.matrix.converter.state.computes.common.ColumnProcessor;
@@ -18,10 +16,9 @@ public class ConnectorDuplicateColumnProperty {
 	private static final String YAML_TYPE_VALUE = "duplicateColumn";
 
 	public static Set<IConnectorStateConverter> getConnectorProperties() {
-		return Stream.of(
-				new ComputeTypeProcessor(HDF_TYPE_VALUE, YAML_TYPE_VALUE),
-				new ColumnProcessor()
-			)
-			.collect(Collectors.toSet());
+		return Set.of(
+			new ComputeTypeProcessor(HDF_TYPE_VALUE, YAML_TYPE_VALUE),
+			new ColumnProcessor()
+		);
 	}
 }
