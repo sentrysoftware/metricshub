b2a361647f7bef4f42e85977bfe89b44870a1d70
Nassim Boutekedjiret <nassim@sentrysoftware.com>
2023-03-27 12:10:35 +0200

    MTX-155
    Keep Matching Lines implementation.

--- hdf-to-yaml-converter/src/main/java/com/sentrysoftware/matrix/converter/state/computes/keeponlymatchinglines/ConnectorKeepOnlyMatchingLinesProperty.java
+++ hdf-to-yaml-converter/src/main/java/com/sentrysoftware/matrix/converter/state/computes/keeponlymatchinglines/ConnectorKeepOnlyMatchingLinesProperty.java
@@ -1,13 +1,12 @@
 package com.sentrysoftware.matrix.converter.state.computes.keeponlymatchinglines;
 
-import java.util.Collections;
 import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import com.sentrysoftware.matrix.converter.state.IConnectorStateConverter;
 import com.sentrysoftware.matrix.converter.state.computes.common.ColumnProcessor;
 import com.sentrysoftware.matrix.converter.state.computes.common.ComputeTypeProcessor;
+import com.sentrysoftware.matrix.converter.state.computes.common.RegexpProcessor;
+import com.sentrysoftware.matrix.converter.state.computes.common.ValueListProcessor;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
@@ -15,10 +14,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ConnectorKeepOnlyMatchingLinesProperty {
 
-	private static final String HDF_TYPE_VALUE = "";
-	private static final String YAML_TYPE_VALUE = "";
+	private static final String HDF_TYPE_VALUE = "KeepMatchingLines";
+	private static final String YAML_TYPE_VALUE = "keepMatchingLines";
 
 	public static Set<IConnectorStateConverter> getConnectorProperties() {
-		return Collections.emptySet();
+		return Set.of(
+			new ComputeTypeProcessor(HDF_TYPE_VALUE, YAML_TYPE_VALUE),
+			new RegexpProcessor(),
+			new ValueListProcessor(),
+			new ColumnProcessor()
+		);
 	}
 }
