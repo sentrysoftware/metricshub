3c513f8aaef7520a4e47d2c4c101ee70b4574944
Fadhela Kerdjoudj <fadhela@sentrysoftware.com>
2023-03-28 16:11:31 +0200

    MTX-182
    add test for NetworkCard discovery instance

---
+++ hdf-to-yaml-converter/src/test/java/com/sentrysoftware/matrix/converter/state/monitors/discovery/instance/NetworkCardDiscoveryInstanceTest.java
@@ -0,0 +1,22 @@
+package com.sentrysoftware.matrix.converter.state.monitors.discovery.instance;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import com.sentrysoftware.matrix.converter.AbstractConnectorPropertyConverterTest;
+
+public class NetworkCardDiscoveryInstanceTest extends AbstractConnectorPropertyConverterTest {
+
+	@Override
+	protected String getResourcePath() {
+		return "src/test/resources/test-files/monitors/discovery/instance/networkCard";
+	}
+
+	@Test
+	@Disabled("Until NetworkCardInstance processor is up")
+	void test() throws IOException {
+		testAll();
+	}
+}
