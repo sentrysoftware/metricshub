14e7b94af0962a16dbe6cfafad6f65ec1916501a
Melinda <melinda@sentrysoftware.com>
2023-03-28 14:27:44 -0400

    MTX-186

--- hdf-to-yaml-converter/src/test/java/com/sentrysoftware/matrix/converter/state/monitors/discovery/instance/RoboticDiscoveryInstanceTest.java
+++ hdf-to-yaml-converter/src/test/java/com/sentrysoftware/matrix/converter/state/monitors/discovery/instance/RoboticDiscoveryInstanceTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import com.sentrysoftware.matrix.converter.AbstractConnectorPropertyConverterTest;
 
-public class RoboticDiscoveryInstanceTest extends AbstractConnectorPropertyConverterTest {
+class RoboticDiscoveryInstanceTest extends AbstractConnectorPropertyConverterTest {
 
 	@Override
 	protected String getResourcePath() {
