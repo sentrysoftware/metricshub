61ae31572675e335f795a8c931c6b9a0571c92b4
Melinda <melinda@sentrysoftware.com>
2023-03-28 14:07:49 -0400

    MTX-176
    remove parameteractivation from input

--- hdf-to-yaml-converter/src/test/java/com/sentrysoftware/matrix/converter/state/monitors/discovery/instance/FanDiscoveryInstanceTest.java
+++ hdf-to-yaml-converter/src/test/java/com/sentrysoftware/matrix/converter/state/monitors/discovery/instance/FanDiscoveryInstanceTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import com.sentrysoftware.matrix.converter.AbstractConnectorPropertyConverterTest;
 
-public class FanDiscoveryInstanceTest extends AbstractConnectorPropertyConverterTest {
+class FanDiscoveryInstanceTest extends AbstractConnectorPropertyConverterTest {
 
 	@Override
 	protected String getResourcePath() {
