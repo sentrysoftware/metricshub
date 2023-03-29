5fda1db40f18310b047259f0cb533385bce6b5b4
Nassim Boutekedjiret <nassim@sentrysoftware.com>
2023-03-29 10:43:21 +0200

    MTX-185 Removed public modifier from JUnit5 test class

--- hdf-to-yaml-converter/src/test/java/com/sentrysoftware/matrix/converter/state/monitors/discovery/instance/PowerSupplyDiscoveryInstanceTest.java
+++ hdf-to-yaml-converter/src/test/java/com/sentrysoftware/matrix/converter/state/monitors/discovery/instance/PowerSupplyDiscoveryInstanceTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import com.sentrysoftware.matrix.converter.AbstractConnectorPropertyConverterTest;
 
-public class PowerSupplyDiscoveryInstanceTest extends AbstractConnectorPropertyConverterTest {
+class PowerSupplyDiscoveryInstanceTest extends AbstractConnectorPropertyConverterTest {
 
 	@Override
 	protected String getResourcePath() {
