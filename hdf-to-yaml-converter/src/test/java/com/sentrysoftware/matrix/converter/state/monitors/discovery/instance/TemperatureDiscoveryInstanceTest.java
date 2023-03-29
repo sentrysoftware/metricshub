7a688b72d70d7615bd0f935f0ee9dc0edd18f246
Fadhela Kerdjoudj <fadhela@sentrysoftware.com>
2023-03-29 10:47:08 +0200

    MTX-188
    Remove public

--- hdf-to-yaml-converter/src/test/java/com/sentrysoftware/matrix/converter/state/monitors/discovery/instance/TemperatureDiscoveryInstanceTest.java
+++ hdf-to-yaml-converter/src/test/java/com/sentrysoftware/matrix/converter/state/monitors/discovery/instance/TemperatureDiscoveryInstanceTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import com.sentrysoftware.matrix.converter.AbstractConnectorPropertyConverterTest;
 
-public class TemperatureDiscoveryInstanceTest extends AbstractConnectorPropertyConverterTest {
+class TemperatureDiscoveryInstanceTest extends AbstractConnectorPropertyConverterTest {
 
 	@Override
 	protected String getResourcePath() {
