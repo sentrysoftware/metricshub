323391c640a9ad08f4d24a5c5048e3502de301fb
Nassim Boutekedjiret <nassim@sentrysoftware.com>
2023-03-29 10:48:29 +0200

    MTX-184 Removed public modifier from JUnit5 test class

--- hdf-to-yaml-converter/src/test/java/com/sentrysoftware/matrix/converter/state/monitors/discovery/instance/PhysicalDiskDiscoveryInstanceTest.java
+++ hdf-to-yaml-converter/src/test/java/com/sentrysoftware/matrix/converter/state/monitors/discovery/instance/PhysicalDiskDiscoveryInstanceTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import com.sentrysoftware.matrix.converter.AbstractConnectorPropertyConverterTest;
 
-public class PhysicalDiskDiscoveryInstanceTest extends AbstractConnectorPropertyConverterTest {
+class PhysicalDiskDiscoveryInstanceTest extends AbstractConnectorPropertyConverterTest {
 
 	@Override
 	protected String getResourcePath() {
