2ff45ffd69a7f8c3978ca08849863e670b3b88e4
Alex <alex@sentrysoftware.com>
2023-03-29 01:45:11 -0400

    MTX-199
    Implemented Led ValueTable tests

---
+++ hdf-to-yaml-converter/src/test/java/com/sentrysoftware/matrix/converter/state/collect/valueTable/LedCollectValueTableTest.java
@@ -0,0 +1,22 @@
+package com.sentrysoftware.matrix.converter.state.collect.valueTable;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import com.sentrysoftware.matrix.converter.AbstractConnectorPropertyConverterTest;
+
+class LedCollectValueTableTest extends AbstractConnectorPropertyConverterTest{
+	
+	@Override
+	protected String getResourcePath() {
+		return "src/test/resources/test-files/monitors/collect/valueTable/led";
+	}
+
+	@Test
+	@Disabled("Until LedCoreValueTable processor is up")
+	void test() throws IOException {
+		testAll();
+	}
+}
