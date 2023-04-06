e8493c62fedfc38989f64b604d79808c8cddec19
Melinda <melinda@sentrysoftware.com>
2023-04-06 16:21:17 -0400

    MTX-236
    post conversion test

--- hdf-to-yaml-converter/src/test/java/com/sentrysoftware/matrix/converter/ConnectorLibraryConverterTest.java
+++ hdf-to-yaml-converter/src/test/java/com/sentrysoftware/matrix/converter/ConnectorLibraryConverterTest.java
@@ -4,14 +4,21 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sentrysoftware.matrix.common.helpers.JsonHelper;
 
 class ConnectorLibraryConverterTest {
 
 	private static final String HDF_DIRECTORY = "src/test/resources/hdf";
+	private static final String YAML_DIRECTORY = "src/test/resources/yaml";
 
 	@TempDir
 	private Path tempDir;
@@ -22,6 +29,30 @@ class ConnectorLibraryConverterTest {
 		processor.process();
 		final File file = tempDir.resolve("DellOpenManage.yaml").toFile();
 		assertTrue(file.exists());
+	}
+
+	@Test
+	@Disabled("Until comments processor is up.")
+	void testProcessComments() throws IOException {
+		final ObjectMapper mapper = JsonHelper.buildYamlMapper();
+		
+		final ConnectorLibraryConverter processor = new ConnectorLibraryConverter(Path.of(HDF_DIRECTORY), tempDir);
+		processor.process();
+
+		final Path expectedPath = Path.of(YAML_DIRECTORY, "DellOpenManage.yaml");
+
+		final File inputFile = tempDir.resolve("DellOpenManage.yaml").toFile();
+		final File expectedFile = expectedPath.toFile();
+		
+		assertTrue(inputFile.exists());
+		assertTrue(expectedFile.exists());
+
+		assertEquals(mapper.readTree(expectedFile), mapper.readTree(inputFile));
+
+		final List<String> inputLines = Files.readAllLines(Path.of(inputFile.getAbsolutePath()));
+		final List<String> expectedLines = Files.readAllLines(expectedPath);
+
+		assertEquals(expectedLines, inputLines);
 	}
 
 }
