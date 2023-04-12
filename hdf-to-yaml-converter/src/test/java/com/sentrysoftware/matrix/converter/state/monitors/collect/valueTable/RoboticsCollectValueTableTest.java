d824a97e560819667aab1cb0c73a9cb3cd5f67a0
Alex <alex@sentrysoftware.com>
2023-04-12 11:19:14 -0400

    MTX-207
    Updated to match mapping convention

--- hdf-to-yaml-converter/src/test/java/com/sentrysoftware/matrix/converter/state/monitors/collect/valueTable/RoboticsCollectValueTableTest.java
+++ hdf-to-yaml-converter/src/test/java/com/sentrysoftware/matrix/converter/state/monitors/collect/valueTable/RoboticsCollectValueTableTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import com.sentrysoftware.matrix.converter.AbstractConnectorPropertyConverterTest;
 
-public class RoboticsCollectValueTableTest extends AbstractConnectorPropertyConverterTest{
+class RoboticsCollectValueTableTest extends AbstractConnectorPropertyConverterTest{
 	
 	@Override
 	protected String getResourcePath() {
