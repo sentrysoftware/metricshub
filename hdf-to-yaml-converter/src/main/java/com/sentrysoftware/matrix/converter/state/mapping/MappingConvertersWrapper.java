ed2f98e12d4ca7b0fe72eab63561ce9b3cd6ae36
Alex <alex@sentrysoftware.com>
2023-04-20 17:10:28 -0400

    MTX-252
    Implemented conversion. Multiple tests for names remain to be implemented.

--- hdf-to-yaml-converter/src/main/java/com/sentrysoftware/matrix/converter/state/mapping/MappingConvertersWrapper.java
+++ hdf-to-yaml-converter/src/main/java/com/sentrysoftware/matrix/converter/state/mapping/MappingConvertersWrapper.java
@@ -58,7 +58,7 @@ public class MappingConvertersWrapper {
 		Map.entry(YAML_LED, NOOP),
 		Map.entry(YAML_LOGICAL_DISK, NOOP),
 		Map.entry(YAML_LUN, NOOP),
-		Map.entry(YAML_MEMORY, NOOP),
+		Map.entry(YAML_MEMORY, new MemoryConverter()),
 		Map.entry(YAML_NETWORK, NOOP),
 		Map.entry(YAML_OTHER_DEVICE, NOOP),
 		Map.entry(YAML_PHYSICAL_DISK, NOOP),
--- hdf-to-yaml-converter/src/test/java/com/sentrysoftware/matrix/converter/state/monitors/collect/valuetable/MemoryCollectValueTableTest.java
+++ hdf-to-yaml-converter/src/test/java/com/sentrysoftware/matrix/converter/state/monitors/collect/valuetable/MemoryCollectValueTableTest.java
@@ -6,8 +6,8 @@ import org.junit.jupiter.api.Test;
 
 import com.sentrysoftware.matrix.converter.AbstractConnectorPropertyConverterTest;
 
-class MemoryCollectValueTableTest extends AbstractConnectorPropertyConverterTest{
-	
+class MemoryCollectValueTableTest extends AbstractConnectorPropertyConverterTest {
+
 	@Override
 	protected String getResourcePath() {
 		return "src/test/resources/test-files/monitors/collect/valueTable/memory";
