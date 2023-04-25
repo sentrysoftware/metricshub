2fefd37302e1316bd25821ac64875497e7e5e8cd
Alex <alex@sentrysoftware.com>
2023-04-25 19:38:53 -0400

    MTX-261
    Added name tests in voltage conversion

--- hdf-to-yaml-converter/src/main/java/com/sentrysoftware/matrix/converter/state/mapping/VoltageConverter.java
+++ hdf-to-yaml-converter/src/main/java/com/sentrysoftware/matrix/converter/state/mapping/VoltageConverter.java
@@ -7,6 +7,7 @@ import static com.sentrysoftware.matrix.converter.ConverterConstants.HDF_LOWER_T
 import static com.sentrysoftware.matrix.converter.ConverterConstants.HDF_STATUS;
 import static com.sentrysoftware.matrix.converter.ConverterConstants.HDF_STATUS_INFORMATION;
 import static com.sentrysoftware.matrix.converter.ConverterConstants.HDF_UPPER_THRESHOLD;
+import static com.sentrysoftware.matrix.converter.ConverterConstants.HDF_VOLTAGE_TYPE;
 import static com.sentrysoftware.matrix.converter.ConverterConstants.HDF_VOLTAGE_VALUE;
 import static com.sentrysoftware.matrix.converter.ConverterConstants.LEGACY_TEXT_PARAMETERS;
 import static com.sentrysoftware.matrix.converter.ConverterConstants.METRICS;
@@ -17,14 +18,16 @@ import static com.sentrysoftware.matrix.converter.ConverterConstants.YAML_STATUS
 import static com.sentrysoftware.matrix.converter.ConverterConstants.YAML_VOLTAGE_HIGH_DEGRADED;
 import static com.sentrysoftware.matrix.converter.ConverterConstants.YAML_VOLTAGE_LOW_CRITICAL;
 import static com.sentrysoftware.matrix.converter.ConverterConstants.YAML_VOLTAGE_STATUS;
-import static com.sentrysoftware.matrix.converter.ConverterConstants.YAML_VOLTAGE_VALUE;
 import static com.sentrysoftware.matrix.converter.ConverterConstants.YAML_VOLTAGE_TYPE;
-import static com.sentrysoftware.matrix.converter.ConverterConstants.HDF_VOLTAGE_TYPE;
+import static com.sentrysoftware.matrix.converter.ConverterConstants.YAML_VOLTAGE_VALUE;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
@@ -65,17 +68,64 @@ public class VoltageConverter extends AbstractMappingConverter {
 
 	@Override
 	protected void setName(ObjectNode existingAttributes, ObjectNode newAttributes) {
-		JsonNode deviceId = existingAttributes.get(HDF_DEVICE_ID);
+		final JsonNode deviceId = existingAttributes.get(HDF_DEVICE_ID);
 		if (deviceId == null) {
 			throw new IllegalStateException(String.format("%s cannot be null.", HDF_DEVICE_ID));
 		}
 
 		final JsonNode displayId = existingAttributes.get(HDF_DISPLAY_ID);
+		JsonNode firstDisplayArgument = deviceId;
+		if (displayId != null) {
+			firstDisplayArgument = displayId;
+		}
+
+		final JsonNode voltageType = existingAttributes.get(HDF_VOLTAGE_TYPE);
 
 		newAttributes.set(
-				YAML_NAME,
-				new TextNode((displayId != null ? displayId : deviceId).asText())
-				);
+			YAML_NAME,
+			new TextNode(
+				buildNameValue(firstDisplayArgument, voltageType)
+			)
+		);
+	}
+
+	/**
+	 * Joins the given non-empty text nodes to build the voltage name value
+	 *
+	 * @param firstDisplayArgument {@link JsonNode} representing the display name
+	 * @param voltageTypeNode       {@link JsonNode} voltage type
+	 *
+	 * @return {@link String} Joined text nodes
+	 */
+	private String buildNameValue(final JsonNode firstDisplayArgument, final JsonNode voltageTypeNode) {
+
+		final String firstArg = firstDisplayArgument.asText();
+		if (voltageTypeNode == null) {
+			return firstArg;
+		}
+
+		// Create the function with the arguments
+		final StringBuilder format = new StringBuilder("sprintf(\"%s (%s)");
+
+		// Build the list of arguments non null
+		final List<String> sprintfArgs = new ArrayList<>();
+		sprintfArgs.add(firstArg);
+		sprintfArgs.add(voltageTypeNode.asText());
+
+		// Join the arguments: $column(1), $column(2)) 
+		// append the result to our format variable in order to get something like
+		// sprint("%s (%s)", $column(1), $column(2))
+		return format
+			.append("\", ") // Here we will have a string like sprintf("%s (%s)", 
+			.append(
+				sprintfArgs
+					.stream()
+					.map(this::getFunctionArgument)
+					.collect(Collectors.joining(", ", "", ")"))
+			)
+			.toString();
+		
+		
 	}
 
 	@Override
