4a757889e7dd8b88c39fe5c8031d3d9eef41f2f8
maalej <maalej@sentrysoftware.com>
2023-07-28 12:09:00 +0200

    MTX-318 Engine - Setup Telemetry Manager data model * Update ConnectorStore

--- matrix-engine/src/main/java/com/sentrysoftware/matrix/connector/model/ConnectorStore.java
+++ matrix-engine/src/main/java/com/sentrysoftware/matrix/connector/model/ConnectorStore.java
@@ -2,6 +2,7 @@ package com.sentrysoftware.matrix.connector.model;
 
 import com.sentrysoftware.matrix.connector.parser.ConnectorLibraryParser;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
@@ -13,20 +14,24 @@ import java.util.Map;
 public class ConnectorStore {
 
 	@Getter
-	private Map<String, Connector> connectors;
+	private Map<String, Connector> store;
+
+	@Getter
+	private Path connectorDirectory;
 
 	public ConnectorStore(Path connectorDirectory) {
 		try {
-			connectors = deserializeConnectors(connectorDirectory);
+			this.connectorDirectory = connectorDirectory;
+			store = deserializeConnectors();
 		} catch (Exception e) {
 			log.error("Error while deserializing connectors. The ConnectorStore is empty!");
 			log.debug("Error while deserializing connectors. The ConnectorStore is empty!", e);
-			connectors = new HashMap<>();
+			store = new HashMap<>();
 		}
 
 	}
 
-	private Map<String, Connector> deserializeConnectors(Path connectorDirectory) throws IOException {
+	private Map<String, Connector> deserializeConnectors() throws IOException {
 		final ConnectorLibraryParser connectorLibraryParser = new ConnectorLibraryParser();
 		return connectorLibraryParser.parseConnectorsFromAllYamlFiles(connectorDirectory);
 	}
