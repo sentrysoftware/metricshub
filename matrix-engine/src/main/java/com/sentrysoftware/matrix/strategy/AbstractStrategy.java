4a20e18f470d8348979c2b1ea9a4f8bb45519a01
Thomas Nohaic <thomas@sentrysoftware.com>
2023-08-01 09:48:37 +0200

    MTX-321
    Correction after PR review

--- matrix-engine/src/main/java/com/sentrysoftware/matrix/strategy/AbstractStrategy.java
+++ matrix-engine/src/main/java/com/sentrysoftware/matrix/strategy/AbstractStrategy.java
@@ -2,6 +2,9 @@ package com.sentrysoftware.matrix.strategy;
 
 import com.sentrysoftware.matrix.telemetry.TelemetryManager;
 
+import lombok.Data;
+
+@Data
 public abstract class AbstractStrategy implements IStrategy, Runnable {
 
 	protected TelemetryManager telemetryManager;
--- matrix-engine/src/main/java/com/sentrysoftware/matrix/strategy/detection/DetectionStrategy.java
+++ matrix-engine/src/main/java/com/sentrysoftware/matrix/strategy/detection/DetectionStrategy.java
@@ -1,8 +1,20 @@
 package com.sentrysoftware.matrix.strategy.detection;
 
 import com.sentrysoftware.matrix.strategy.AbstractStrategy;
+import com.sentrysoftware.matrix.telemetry.TelemetryManager;
 
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@Builder
 public class DetectionStrategy extends AbstractStrategy {
+
+	public DetectionStrategy(final TelemetryManager telemetryManager) {
+		this.telemetryManager = telemetryManager;
+	}
 
 	@Override
 	public void run() {
