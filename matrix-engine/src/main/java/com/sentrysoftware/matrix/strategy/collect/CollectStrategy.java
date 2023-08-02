f4d5136b5453c41cc13ba3abdfc4b9ac617fe0ab
Thomas Nohaic <thomas@sentrysoftware.com>
2023-08-02 09:42:08 +0200

    MTX-323
    Collect strategy setup

---
+++ matrix-engine/src/main/java/com/sentrysoftware/matrix/strategy/collect/CollectStrategy.java
@@ -0,0 +1,24 @@
+package com.sentrysoftware.matrix.strategy.collect;
+
+import com.sentrysoftware.matrix.strategy.AbstractStrategy;
+
+public class CollectStrategy extends AbstractStrategy {
+
+	@Override
+	public void prepare() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void post() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+}
