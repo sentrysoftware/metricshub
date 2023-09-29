029232fd5c27db7080c8de633b052220d5898218
maalej <maalej@sentrysoftware.com>
2023-09-29 10:46:03 +0200

    MTX-366
    Implement Wbem Source Update and Execution
    
    * Develop WbemSource processing
    
    * Implement WbemSource processing tests

--- matrix-engine/src/test/java/com/sentrysoftware/matrix/strategy/source/SourceUpdaterProcessorTest.java
+++ matrix-engine/src/test/java/com/sentrysoftware/matrix/strategy/source/SourceUpdaterProcessorTest.java
@@ -1,5 +1,6 @@
 package com.sentrysoftware.matrix.strategy.source;
 
+import static com.sentrysoftware.matrix.constants.Constants.EMPTY;
 import static com.sentrysoftware.matrix.constants.Constants.ENCLOSURE_COLLECT_SOURCE_1;
 import static com.sentrysoftware.matrix.constants.Constants.EXPECTED_RESULT;
 import static com.sentrysoftware.matrix.constants.Constants.EXPECTED_SNMP_TABLE_DATA;
@@ -41,6 +42,7 @@ import com.sentrysoftware.matrix.connector.model.monitor.task.source.SnmpTableSo
 import com.sentrysoftware.matrix.connector.model.monitor.task.source.StaticSource;
 import com.sentrysoftware.matrix.connector.model.monitor.task.source.TableJoinSource;
 import com.sentrysoftware.matrix.connector.model.monitor.task.source.TableUnionSource;
+import com.sentrysoftware.matrix.connector.model.monitor.task.source.WbemSource;
 import com.sentrysoftware.matrix.telemetry.HostProperties;
 import com.sentrysoftware.matrix.telemetry.TelemetryManager;
 import java.util.ArrayList;
@@ -344,5 +346,12 @@ class SourceUpdaterProcessorTest {
 			new SourceUpdaterProcessor(sourceProcessor, telemetryManager, MY_CONNECTOR_1_NAME, MONITOR_ID_ATTRIBUTE_VALUE)
 				.process(staticSource)
 		);
+	}
+
+	@Test
+	void testProcessWbemSource() {
+		final TelemetryManager telemetryManager = TelemetryManager.builder().hostConfiguration(HostConfiguration.builder().build()).build();
+		doReturn(SourceTable.empty()).when(sourceProcessor).process(any(WbemSource.class));
+		assertEquals(SourceTable.empty(), new SourceUpdaterProcessor(sourceProcessor, telemetryManager, MY_CONNECTOR_1_NAME, MONITOR_ID_ATTRIBUTE_VALUE).process(WbemSource.builder().query(EMPTY).build()));
 	}
 }
