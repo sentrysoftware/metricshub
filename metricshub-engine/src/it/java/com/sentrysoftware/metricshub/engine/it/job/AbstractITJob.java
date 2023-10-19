fc3451bd3649e9e19ae24056f1aef1cf05c71ab5
Melinda <melinda@sentrysoftware.com>
2023-10-19 10:27:51 -0400

    MTX-422
    ITJob and AbstractITJob WIP for review

---
+++ metricshub-engine/src/it/java/com/sentrysoftware/metricshub/engine/it/job/AbstractITJob.java
@@ -0,0 +1,282 @@
+package com.sentrysoftware.metricshub.engine.it.job;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map.Entry;
+
+import com.sentrysoftware.metricshub.engine.alert.AlertRule;
+import com.sentrysoftware.metricshub.engine.common.helpers.JsonHelper;
+import com.sentrysoftware.metricshub.engine.matsya.MatsyaClientsExecutor;
+import com.sentrysoftware.metricshub.engine.strategy.IStrategy;
+import com.sentrysoftware.metricshub.engine.strategy.collect.CollectStrategy;
+import com.sentrysoftware.metricshub.engine.strategy.collect.PostCollectStrategy;
+import com.sentrysoftware.metricshub.engine.strategy.collect.PrepareCollectStrategy;
+import com.sentrysoftware.metricshub.engine.strategy.detection.DetectionStrategy;
+import com.sentrysoftware.metricshub.engine.strategy.discovery.DiscoveryStrategy;
+import com.sentrysoftware.metricshub.engine.strategy.discovery.PostDiscoveryStrategy;
+import com.sentrysoftware.metricshub.engine.strategy.simple.SimpleStrategy;
+import com.sentrysoftware.metricshub.engine.telemetry.Monitor;
+import com.sentrysoftware.metricshub.engine.telemetry.MonitorsVo;
+import com.sentrysoftware.metricshub.engine.telemetry.TelemetryManager;
+import com.sentrysoftware.metricshub.engine.telemetry.metric.AbstractMetric;
+
+import lombok.Data;
+import lombok.NonNull;
+
+@Data
+public abstract class AbstractITJob implements ITJob {
+
+	@NonNull
+	private final MatsyaClientsExecutor matsyaClientsExecutor;
+
+	@NonNull
+	private final TelemetryManager telemetryManager;
+
+	/**
+	 * Assert that expected and actual are equal.
+	 *
+	 * @param expected
+	 * @param actual
+	 */
+	private static void assertMonitor(final Monitor expected, final Monitor actual) {
+
+		assertMetrics(expected, actual);
+		assertMonitorAttributes(expected, actual);
+		assertConditionalCollection(expected, actual);
+		assertLegacyTextParameters(expected, actual);
+		assertAlertRules(expected, actual);
+		assertEquals(expected.getResource(), actual.getResource());
+		assertEquals(expected.getDiscoveryTime(), actual.getDiscoveryTime());
+		assertEquals(expected.getType(), actual.getType(), "Type doesn't match actual Type on monitor: " + expected.getId());
+		assertEquals(expected.getId(), actual.getId(), () -> "ID doesn't match actual ID on monitor: " + expected.getId());
+		assertEquals(expected.isEndpoint(), actual.isEndpoint(), () -> "isEndpoint doesn't match actual isEndpoint on monitor: " + expected.getId());
+	}
+
+	/**
+	 * Assert that expected and actual alert rules are equal. <br>
+	 * We only test testable/important data. For example the {@link AlertRule} conditionsChecker cannot be checked as it is a function
+	 *
+	 * @param expected
+	 * @param actual
+	 */
+	private static void assertAlertRules(final Monitor expected, final Monitor actual) {
+		for (final Entry<String, List<AlertRule>> expectedEntry : expected.getAlertRules().entrySet()) {
+			final List<AlertRule> expectedAlertRules = expectedEntry.getValue();
+			final String parameterName = expectedEntry.getKey();
+
+			assertNotNull(expectedAlertRules, () -> "Expected alert rules cannot be null for monitor identifier: " + expected.getId());
+			for (AlertRule expectedAlertRule : expectedAlertRules) {
+
+				final List<AlertRule> actualAlertRules = actual.getAlertRules().get(parameterName);
+
+				assertNotNull(actualAlertRules, 
+						() -> "Cannot find actual alert rules on monitor: " + expected.getId() + ". For parameter: "
+								+ parameterName);
+
+				final AlertRule actualAlertRule = actualAlertRules.stream()
+						.filter(rule -> rule.same(expectedAlertRule)).findFirst().orElse(null);
+
+				assertNotNull(actualAlertRule,
+						() -> "Cannot find actual alert rule on monitor: " + expected.getId() + ". For parameter: "
+								+ parameterName);
+
+				assertEquals(expectedAlertRule.getActive(), actualAlertRule.getActive(),
+						() -> "Alert Rule Active doesn’t match actual value on monitor: " + expected.getId() + ". For parameter: "
+								+ parameterName);
+
+				assertEquals(expectedAlertRule.getConditions(), actualAlertRule.getConditions(),
+						() -> "Alert Rule Conditions doesn’t match actual conditions on monitor: " + expected.getId() + ". For parameter: "
+								+ parameterName);
+
+				assertEquals(expectedAlertRule.getSeverity(), actualAlertRule.getSeverity(),
+						() -> "Alert Rule Severity doesn’t match actual severity on monitor: " + expected.getId() + ". For parameter: "
+								+ parameterName);
+
+				assertEquals(expectedAlertRule.getDetails(), actualAlertRule.getDetails(),
+						() -> "Alert Rule Details don't match actual details on monitor: " + expected.getId() + ". For parameter: "
+								+ parameterName);
+				assertEquals(expectedAlertRule.getType(), actualAlertRule.getType(),
+						() -> "Alert Rule Type doesn't match actual type on monitor: " + expected.getId() + ". For parameter: "
+								+ parameterName);
+
+				assertEquals(expectedAlertRule.getPeriod(), actualAlertRule.getPeriod(),
+						() -> "Alert Rule Period doesn't match actual type on monitor: " + expected.getId() + ". For parameter: "
+								+ parameterName);
+			}
+		}
+	}
+
+	/**
+	 * Assert that expected and actual metrics are equal. <br>
+	 * We only test testable/important data. For example the {@link Metric} conditionsChecker cannot be checked as it is a function
+	 *
+	 * @param expected
+	 * @param actual
+	 */
+	private static void assertMetrics(final Monitor expected, final Monitor actual) {
+
+		for (final Entry<String, AbstractMetric> expectedEntry : expected.getMetrics().entrySet()) {
+		
+			final AbstractMetric expectedMetric = expectedEntry.getValue();
+			final String expectedKey = expectedEntry.getKey();
+
+			assertNotNull(expectedMetric, () -> "Expected metric cannot be null for monitor identifier: " + expected.getId());
+
+			final AbstractMetric actualMetric = actual.getMetric(expectedMetric.getName(), expectedMetric.getClass());
+
+			assertNotNull(actualMetric, 
+					() -> "Cannot find actual metric on monitor: " + expected.getId() + ". For parameter: "
+							+ expectedKey);
+
+			assertEquals(expectedMetric.getName(), actualMetric.getName(),
+					"Name doesn’t match actual value on monitor: " + expected.getId() + ". For parameter: "
+							+ expectedMetric.getName());
+
+			assertEquals(expectedMetric.getCollectTime(), actualMetric.getCollectTime(), 
+					"CollectTime doesn’t match actual collect time on monitor: " + expected.getId() + ". For parameter: "
+							+ expectedMetric.getName());
+
+			assertEquals(expectedMetric.getPreviousCollectTime(), actualMetric.getPreviousCollectTime(),
+					"PreviousCollectTime doesn’t match actual previous collect time on monitor: " + expected.getId() + ". For parameter: "
+							+ expectedMetric.getName());
+
+			assertMetricAttributes(expectedMetric, actualMetric);
+
+			assertEquals(expectedMetric.isResetMetricTime(), actualMetric.isResetMetricTime(),
+					() -> "IsResetMetricTime doesn't match actual isResetMetricTime on monitor: " + expected.getId() + ". For parameter: "
+							+ expectedMetric.getName());
+		}
+	}
+
+	private static void assertMonitorAttributes(final Monitor expectedMonitor, final Monitor actualMonitor) {
+		for (final Entry<String, String> expectedEntry : expectedMonitor.getAttributes().entrySet()) {
+		
+			final String expected = expectedEntry.getValue();
+			final String expectedKey = expectedEntry.getKey();
+
+			assertNotNull(expected, () -> "Expected attribute cannot be null for monitor identifier: " + expectedMonitor.getId());
+
+			final String actual = actualMonitor.getAttribute(expectedKey);
+
+			assertNotNull(actual, 
+					() -> "Cannot find actual attribute on monitor: " + expectedMonitor.getId() + ". For parameter: "
+							+ expectedKey);
+
+			assertEquals(expected, actual);
+		}
+	}
+
+	private static void assertMetricAttributes(final AbstractMetric expectedMetric, final AbstractMetric actualMetric) {
+		for (final Entry<String, String> expectedEntry : expectedMetric.getAttributes().entrySet()) {
+		
+			final String expected = expectedEntry.getValue();
+			final String expectedKey = expectedEntry.getKey();
+
+			assertNotNull(expected, () -> "Expected attribute cannot be null for metric identifier: " + expectedMetric.getName());
+
+			final String actual = actualMetric.getAttributes().get(expectedKey);
+
+			assertNotNull(actual, 
+					() -> "Cannot find actual attribute for metric: " + expectedMetric.getName() + ". For parameter: "
+							+ expectedKey);
+
+			assertEquals(expected, actual);
+		}
+	}
+
+	private static void assertConditionalCollection(final Monitor expectedMonitor, final Monitor actualMonitor) {
+		for (final Entry<String, String> expectedEntry : expectedMonitor.getConditionalCollection().entrySet()) {
+		
+			final String expected = expectedEntry.getValue();
+			final String expectedKey = expectedEntry.getKey();
+
+			assertNotNull(expected, () -> "Expected conditional collection cannot be null for monitor identifier: " + expectedMonitor.getId());
+
+			final String actual = actualMonitor.getAttribute(expectedKey);
+
+			assertNotNull(actual, 
+					() -> "Cannot find actual conditional collection on monitor: " + expectedMonitor.getId() + ". For parameter: "
+							+ expectedKey);
+
+			assertEquals(expected, actual);
+		}
+	}
+
+	private static void assertLegacyTextParameters(final Monitor expectedMonitor, final Monitor actualMonitor) {
+		for (final Entry<String, String> expectedEntry : expectedMonitor.getLegacyTextParameters().entrySet()) {
+		
+			final String expected = expectedEntry.getValue();
+			final String expectedKey = expectedEntry.getKey();
+
+			assertNotNull(expected, () -> "Expected Legacy text parameter cannot be null for monitor identifier: " + expectedMonitor.getId());
+
+			final String actual = actualMonitor.getAttribute(expectedKey);
+
+			assertNotNull(actual, 
+					() -> "Cannot find actual legacy text parameter on monitor: " + expectedMonitor.getId() + ". For parameter: "
+							+ expectedKey);
+
+			assertEquals(expected, actual);
+		}
+	}
+
+
+	@Override
+	public ITJob verifyExpected(final String expectedPath) throws Exception {
+
+		stopServer();
+
+		// assertEquals(OperationStatus.SUCCESS, lastEngineResult.getOperationStatus(), "Last strategy failed.");
+
+		final InputStream is = ITJobUtils.getItResourceAsInputStream(expectedPath);
+		final MonitorsVo expected = JsonHelper.deserialize(is, MonitorsVo.class);
+
+		final MonitorsVo actual = telemetryManager.getVo();
+
+		assertEquals(expected.getTotal(), actual.getTotal());
+
+		for (int i = 0; i < expected.getMonitors().size(); i++) {
+			assertMonitor(expected.getMonitors().get(i), actual.getMonitors().get(i));
+		}
+
+		return this;
+	}
+
+	@Override
+	public ITJob executeDiscoveryStrategy(final IStrategy strategy) {
+		
+		final Long discoveryTime = strategy.getStrategyTime();
+
+		telemetryManager.run(
+			new DetectionStrategy(telemetryManager, discoveryTime, matsyaClientsExecutor),
+			new DiscoveryStrategy(telemetryManager, discoveryTime, matsyaClientsExecutor),
+			new SimpleStrategy(telemetryManager, discoveryTime, matsyaClientsExecutor),
+			new PostDiscoveryStrategy(telemetryManager, discoveryTime, matsyaClientsExecutor)
+		);
+
+		assertTrue(isServerStarted(), "Server not ready.");
+
+		return this;
+	}
+
+
+	@Override
+	public ITJob executeCollectStrategy(final IStrategy strategy) {
+		final Long collectTime = strategy.getStrategyTime();
+
+		telemetryManager.run(
+			new PrepareCollectStrategy(telemetryManager, collectTime, matsyaClientsExecutor),
+			new CollectStrategy(telemetryManager, collectTime, matsyaClientsExecutor),
+			new SimpleStrategy(telemetryManager, collectTime, matsyaClientsExecutor),
+			new PostCollectStrategy(telemetryManager, collectTime, matsyaClientsExecutor)
+		);
+
+		assertTrue(isServerStarted(), "Server not ready.");
+
+		return this;
+	}
+}
---
+++ metricshub-engine/src/it/java/com/sentrysoftware/metricshub/engine/it/job/ITJob.java
@@ -0,0 +1,61 @@
+package com.sentrysoftware.metricshub.engine.it.job;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+import com.sentrysoftware.metricshub.engine.strategy.IStrategy;
+
+
+public interface ITJob {
+
+	/**
+	 * Start the server and load the record data from the given paths
+	 *
+	 * @param recordDataPaths Record data located under src/it/resources, the path are expected without src/it/resources
+	 * @return The actual {@link ITJob}
+	 * @throws Exception
+	 */
+	ITJob withServerRecordData(String... recordDataPaths) throws Exception;
+
+	/**
+	 * Run the engine {@link IStrategy}
+	 *
+	 * @return The actual {@link ITJob}
+	 */
+	ITJob executeDiscoveryStrategy(IStrategy strategy);
+
+	/**
+	 * Run the engine {@link IStrategy}
+	 *
+	 * @return The actual {@link ITJob}
+	 */
+	ITJob executeCollectStrategy(IStrategy strategy);
+
+	/**
+	 * Verify the expected result located under the given expectedPath
+	 *
+	 * @param expectedPath Expected result path located under src/it/resources, the path are expected without src/it/resources
+	 * @return actual {@link ITJob}
+	 * @throws Exception
+	 */
+	ITJob verifyExpected(String expectedPath) throws Exception;
+
+	/**
+	 * Stop the server
+	 */
+	void stopServer();
+
+	/**
+	 * @return <code>true</code> if the server is started otherwise <code>false</code>
+	 */
+	boolean isServerStarted();
+
+	/**
+	 * Save the hostMonitoring JSON into a file.
+	 *
+	 * @param path path of the saving file.
+	 * @return actual {@link ITJob}
+	 * @throws IOException
+	 */
+	ITJob saveHostMonitoringJson(final Path path) throws IOException;
+}
