4a352412eb2d3f6c9f1d530f076179bc5997dc38
maalej <maalej@sentrysoftware.com>
2023-11-14 16:53:06 +0100

    MTX-449 Develop SNMP CLI Job * Refactor code after review

--- metricshub-agent/src/test/java/com/sentrysoftware/metricshub/cli/service/MetricsHubCliServiceTest.java
+++ metricshub-agent/src/test/java/com/sentrysoftware/metricshub/cli/service/MetricsHubCliServiceTest.java
@@ -2,16 +2,12 @@ package com.sentrysoftware.metricshub.cli.service;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.sentrysoftware.metricshub.cli.helper.StringBuilderWriter;
 import com.sentrysoftware.metricshub.cli.service.MetricsHubCliService.CliPasswordReader;
-import com.sentrysoftware.metricshub.cli.service.converter.SnmpPrivacyConverter;
-import com.sentrysoftware.metricshub.cli.service.converter.SnmpVersionConverter;
 import com.sentrysoftware.metricshub.cli.service.protocol.IpmiConfigCli;
 import com.sentrysoftware.metricshub.cli.service.protocol.SnmpConfigCli;
-import com.sentrysoftware.metricshub.engine.configuration.SnmpConfiguration;
 import com.sentrysoftware.metricshub.engine.connector.model.Connector;
 import com.sentrysoftware.metricshub.engine.connector.model.ConnectorStore;
 import com.sentrysoftware.metricshub.engine.connector.model.common.DeviceKind;
@@ -238,48 +234,5 @@ class MetricsHubCliServiceTest {
 		// This confirms that tryInteractiveSnmpPassword hasn't triggered the password reader
 		// because the both username and password are already present in snmpConfigCli
 		assertTrue(builder.isEmpty());
-	}
-
-	@Test
-	void testSnmpVersionConverter() {
-		final SnmpVersionConverter snmpVersionConverter = new SnmpVersionConverter();
-
-		// Check Snmp version conversion
-		assertEquals(SnmpConfiguration.SnmpVersion.V3_MD5, snmpVersionConverter.convert("v3 md5"));
-		assertEquals(SnmpConfiguration.SnmpVersion.V3_NO_AUTH, snmpVersionConverter.convert("v3 no auth"));
-		assertEquals(SnmpConfiguration.SnmpVersion.V2C, snmpVersionConverter.convert("v2"));
-		assertEquals(SnmpConfiguration.SnmpVersion.V1, snmpVersionConverter.convert("v1"));
-	}
-
-	@Test
-	void testSnmpPrivacyConverter() {
-		final SnmpPrivacyConverter snmpPrivacyConverter = new SnmpPrivacyConverter();
-
-		// Check Snmp privacy conversion
-		assertEquals(SnmpConfiguration.Privacy.DES, snmpPrivacyConverter.convert("des"));
-		assertEquals(SnmpConfiguration.Privacy.AES, snmpPrivacyConverter.convert("aes"));
-		assertEquals(SnmpConfiguration.Privacy.NO_ENCRYPTION, snmpPrivacyConverter.convert("none"));
-		assertEquals(SnmpConfiguration.Privacy.NO_ENCRYPTION, snmpPrivacyConverter.convert("no"));
-	}
-
-	@Test
-	void testSnmpConfigCliToProtocol() {
-		// Create a SnmpConfigCli instance and set its data
-		final SnmpConfigCli snmpConfigCli = new SnmpConfigCli();
-		snmpConfigCli.setSnmpVersion(SnmpConfiguration.SnmpVersion.V1);
-		snmpConfigCli.setPort(80);
-		snmpConfigCli.setCommunity("community");
-		snmpConfigCli.setPrivacy(SnmpConfiguration.Privacy.AES);
-		snmpConfigCli.setTimeout(120L);
-		snmpConfigCli.setPrivacyPassword(PASSWORD);
-
-		// Create an SnmpConfiguration and call method toProtocol
-
-		final SnmpConfiguration snmpConfiguration = snmpConfigCli.toProtocol("user1", PASSWORD);
-
-		// Check the resulting snmp configuration
-		assertNotNull(snmpConfiguration);
-		assertEquals(SnmpConfiguration.SnmpVersion.V1, snmpConfiguration.getVersion());
-		assertEquals("SNMP v1 (community)", snmpConfiguration.toString());
 	}
 }
