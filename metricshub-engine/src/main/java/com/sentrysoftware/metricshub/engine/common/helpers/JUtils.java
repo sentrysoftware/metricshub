4d19c348345154714dedfc33edb5ac7a1c905fe4
Kawtar Bakour <kawtar@sentrysoftware.com>
2024-01-15 11:21:45 +0100

    MTX-538
    Import JUtils code in MetricsHub
    
    * Minor modification

--- metricshub-engine/src/main/java/com/sentrysoftware/metricshub/engine/common/helpers/JUtils.java
+++ metricshub-engine/src/main/java/com/sentrysoftware/metricshub/engine/common/helpers/JUtils.java
@@ -4,13 +4,17 @@ import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
-
+@Slf4j
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class JUtils {
 
-	private JUtils() {} // Static only
-
 	/**
+	 * Encodes the input string using the SHA-256 algorithm and returns the result in hexadecimal format.
+	 *
 	 * @param input String to convert
 	 * @return the encoded string in hex
 	 */
@@ -28,6 +32,7 @@ public class JUtils {
 
 			return String.format("%064x", new BigInteger(1, digest));
 		} catch (NoSuchAlgorithmException e) {
+			log.error("Error while encoding SHA-256: {}", e.getMessage());
 			return null;
 		}
 
