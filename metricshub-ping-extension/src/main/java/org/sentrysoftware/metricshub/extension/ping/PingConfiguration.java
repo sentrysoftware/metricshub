8e3ac6147046773fa96c10960436eedcb9ccec2f
Nassim Boutekedjiret <nassim@sentrysoftware.com>
2024-06-04 19:23:20 +0200

    Issue #226: Bad timeout unit used by the ping extension
    * Increased default timeout to 5 seconds.

--- metricshub-ping-extension/src/main/java/org/sentrysoftware/metricshub/extension/ping/PingConfiguration.java
+++ metricshub-ping-extension/src/main/java/org/sentrysoftware/metricshub/extension/ping/PingConfiguration.java
@@ -50,7 +50,7 @@ public class PingConfiguration implements IConfiguration {
 	@Default
 	@JsonSetter(nulls = SKIP)
 	@JsonDeserialize(using = TimeDeserializer.class)
-	private final Long timeout = 2L;
+	private final Long timeout = 5L;
 
 	@Override
 	public void validateConfiguration(String resourceKey) throws InvalidConfigurationException {
