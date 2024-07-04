b8db87ed71ae4e23573a13bc421fd21250d9f3ae
Safae Ajib <safae@sentrysoftware.com>
2024-07-04 15:43:19 +0200

    Issue #238: Hardware Enrichment - Adjust Memory Thresholds * Update unit test

--- metricshub-hardware/src/test/java/org/sentrysoftware/metricshub/hardware/threshold/LogicalDiskMetricNormalizerTest.java
+++ metricshub-hardware/src/test/java/org/sentrysoftware/metricshub/hardware/threshold/LogicalDiskMetricNormalizerTest.java
@@ -1,6 +1,6 @@
 package org.sentrysoftware.metricshub.hardware.threshold;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.HashMap;
 import java.util.Map;
