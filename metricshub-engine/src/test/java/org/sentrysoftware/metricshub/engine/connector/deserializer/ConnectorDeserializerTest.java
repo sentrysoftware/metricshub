91ce6a3e10f41e24b48134aa92f451aedd24f580
maalej <maalej@sentrysoftware.com>
2025-01-02 17:46:25 +0100

    Issue #530: Update connector platforms field * Update the field platforms under ConnectorIdentity (From String to Set of String) * Add PlatformsDeserializer * Update unit tests

--- metricshub-engine/src/test/java/org/sentrysoftware/metricshub/engine/connector/deserializer/ConnectorDeserializerTest.java
+++ metricshub-engine/src/test/java/org/sentrysoftware/metricshub/engine/connector/deserializer/ConnectorDeserializerTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.io.IOException;
+import java.util.Set;
 import org.junit.jupiter.api.Test;
 import org.sentrysoftware.metricshub.engine.connector.model.Connector;
 import org.sentrysoftware.metricshub.engine.connector.model.identity.ConnectorIdentity;
@@ -16,13 +17,26 @@ class ConnectorDeserializerTest extends DeserializerTest {
 	}
 
 	@Test
-	void testDeserializeConnector() throws IOException {
+	void testDeserializeConnectorWithStringPlatformsField() throws IOException {
 		final Connector connector = getConnector("connector");
 		final ConnectorIdentity connectorIdentity = connector.getConnectorIdentity();
 
 		assertNotNull(connector);
 		assertEquals("testConnector", connectorIdentity.getDisplayName());
-		assertEquals("testPlatforms", connectorIdentity.getPlatforms());
+		assertEquals(Set.of("testPlatform1, testPlatform2, testPlatform3"), connectorIdentity.getPlatforms());
+		assertEquals("testReliesOn", connectorIdentity.getReliesOn());
+		assertEquals("1.0.0", connectorIdentity.getVersion());
+		assertEquals("20", connectorIdentity.getProjectVersion());
+		assertEquals("testInformation", connectorIdentity.getInformation());
+	}
+
+	@Test
+	void testDeserializeConnectorWithSetPlatformsField() throws IOException {
+		final Connector connector = getConnector("connector");
+		final ConnectorIdentity connectorIdentity = connector.getConnectorIdentity();
+		assertNotNull(connector);
+		assertEquals("testConnector", connectorIdentity.getDisplayName());
+		assertEquals(Set.of("testPlatform1, testPlatform2, testPlatform3"), connectorIdentity.getPlatforms());
 		assertEquals("testReliesOn", connectorIdentity.getReliesOn());
 		assertEquals("1.0.0", connectorIdentity.getVersion());
 		assertEquals("20", connectorIdentity.getProjectVersion());
