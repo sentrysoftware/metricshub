ad38323651bf60aebd9a550b80d2ad85293f6664
maalej <maalej@sentrysoftware.com>
2025-01-03 17:20:02 +0100

    Issue #530: Update connector platforms field * Update after code review

--- metricshub-engine/src/test/java/org/sentrysoftware/metricshub/engine/connector/deserializer/ConnectorDeserializerTest.java
+++ metricshub-engine/src/test/java/org/sentrysoftware/metricshub/engine/connector/deserializer/ConnectorDeserializerTest.java
@@ -2,9 +2,9 @@ package org.sentrysoftware.metricshub.engine.connector.deserializer;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.LinkedHashSet;
 import java.util.Set;
 import org.junit.jupiter.api.Test;
@@ -20,16 +20,21 @@ class ConnectorDeserializerTest extends DeserializerTest {
 
 	@Test
 	void testDeserializeConnectorWithStringPlatformsField() throws IOException {
+		// Retrieve the connector
 		final Connector connector = getConnector("connector");
 		final ConnectorIdentity connectorIdentity = connector.getConnectorIdentity();
-
 		assertNotNull(connector);
 		assertEquals("testConnector", connectorIdentity.getDisplayName());
+
+		// Compare the actual platforms against the expected platforms
 		final Set<String> expectedPlatforms = new LinkedHashSet<>();
 		expectedPlatforms.add("testPlatform1");
 		expectedPlatforms.add("testPlatform2");
 		expectedPlatforms.add("testPlatform3");
-		assertTrue(expectedPlatforms.containsAll(connectorIdentity.getPlatforms()));
+		final Set<String> actualPlatforms = connectorIdentity.getPlatforms();
+		assertEquals(new ArrayList<>(expectedPlatforms), new ArrayList<>(actualPlatforms), "The order of platforms does not match!");
+
+		// Check other connector identity fields
 		assertEquals("testReliesOn", connectorIdentity.getReliesOn());
 		assertEquals("1.0.0", connectorIdentity.getVersion());
 		assertEquals("20", connectorIdentity.getProjectVersion());
@@ -38,14 +43,20 @@ class ConnectorDeserializerTest extends DeserializerTest {
 
 	@Test
 	void testDeserializeConnectorWithSetPlatformsField() throws IOException {
+		// Retrieve the connector
 		final Connector connector = getConnector("connector");
 		final ConnectorIdentity connectorIdentity = connector.getConnectorIdentity();
 		assertNotNull(connector);
+
+		// Compare the actual platforms against the expected platforms
 		final Set<String> expectedPlatforms = new LinkedHashSet<>();
 		expectedPlatforms.add("testPlatform1");
 		expectedPlatforms.add("testPlatform2");
 		expectedPlatforms.add("testPlatform3");
-		assertTrue(expectedPlatforms.containsAll(connectorIdentity.getPlatforms()));
+		final Set<String> actualPlatforms = connectorIdentity.getPlatforms();
+		assertEquals(new ArrayList<>(expectedPlatforms), new ArrayList<>(actualPlatforms), "The order of platforms does not match!");
+
+		// Check other connector identity fields
 		assertEquals("testReliesOn", connectorIdentity.getReliesOn());
 		assertEquals("1.0.0", connectorIdentity.getVersion());
 		assertEquals("20", connectorIdentity.getProjectVersion());
